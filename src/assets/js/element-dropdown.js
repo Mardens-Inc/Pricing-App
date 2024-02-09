d09643af97d2f3ea3b4b28fb1621fc1f0d845e04
drew-chase <dcmanproductions@gmail.com>
2024-02-09 13:41:05 -0500

    Refactor dropdown menu implementation
    The dropdown menu creation and management have been streamlined. The previous model involved initializing the dropdown via a function, this has been changed to an immediate definition. The way the dropdown is triggered has been simplified and focused on an 'openDropdown' function, which manages item interactions and dropdown positioning.

--- src/assets/js/element-dropdown.js
+++ src/assets/js/element-dropdown.js
@@ -1,67 +1,67 @@
-// Function to initialize a dropdown menu
-function loadDropdown() {
+// Creating a dropdown element
+const dropdown = $(`<div class="dropdown-items" tabindex="-1"> </div>`)
+if ($(".dropdown-items").length === 0) {
+    // Appending the dropdown to the body of the HTML
+    $('body').append(dropdown)
+}
 
-    // Creating a dropdown element
-    const dropdown = $(`<div class="dropdown-items"> </div>`)
+/**
+ * Opens a dropdown menu with the given items.
+ *
+ * @param {jQuery|HTMLElement} element - The element that triggered the dropdown menu.
+ * @param {Object} items - The items to populate the dropdown with.
+ *                       Each item must be an object with a single key-value pair,
+ *                       where the key represents the display name of the item,
+ *                       and the value is a function to be executed when the item is clicked.
+ *                       The function should have no parameters.
+ *                       Example: [{ name: () => {} }, { ... }]
+ *
+ * @return {void} - This method does not return anything.
+ */
+function openDropdown(element, items) {
 
-    if ($(".dropdown-items").length == 0) {
-        // Appending the dropdown to the body of the HTML
-        $('body').append(dropdown)
-    }
+    // Convert the element to a jQuery object
+    element = $(element);
 
-    // Selecting elements that have a dropdown attribute but are not marked
-    const elementsWithDropdowns = $("[dropdown]:not([marked])")
+    // Clear the dropdown
+    dropdown.html("");
 
-    // Iterating over each element and attaching necessary attributes
-    elementsWithDropdowns.each((i, element) => {
-        element = $(element);
-        // marking the element as processed
-        element.attr('marked', true)
-        // Adding a tabindex attribute for focus
-        element.attr('tabindex', 0);
-    });
-
-    // Adding event handler for focus and blur events
-    elementsWithDropdowns.on('focus', showDropdown);
-    elementsWithDropdowns.on('blur', hideDropdown);
-
-    // Function to handle focus event and show dropdown menu
-    function showDropdown(e) {
-        e.preventDefault();
-        // Get the current target of the event
-        const element = $(e.currentTarget)
-        element.addClass("active")
-        // Extract the current dropdown items by splitting the dropdown attribute of the element
-        const items = element.attr('dropdown').split(';')
-        // Populate HTML content inside the dropdown element
-        dropdown.html(items.map(item => `<div class="dropdown-item" onclick="${element.attr(`action-${item.replace(/\s/g, '-')}`)}">${item}</div>`).join(''))
-        dropdown[0].scrollTo({top: 0, behavior: 'auto'});
-        // Logic for positioning the dropdown menu.
-        let x = element.offset().left + element.width() / 2 - dropdown.width() / 2
-        let y = element.offset().top + element.height() + 40
-        if (y + dropdown.height() > window.innerHeight) {
-            y = window.innerHeight - dropdown.height() - 10
-        }
-        if (x + dropdown.width() > window.innerWidth) {
-            x = window.innerWidth - dropdown.width() - 10
-        }
-        // Apply CSS styles to position the dropdown correctly
-        dropdown.css({
-            top: y,
-            left: x,
-            opacity: 1,
-            "pointer-events": "all"
-        })
-    }
-
-    // Function to handle blur event and hide dropdown menu
-    function hideDropdown() {
-        // Delay the hiding process to allow for click events
-        setTimeout(() => {
+    // Populate the dropdown with the items
+    Object.keys(items).forEach(item => {
+        const itemElement = $(`<div class="dropdown-item">${item}</div>`);
+        itemElement.on('click', () => {
+            items[item]();
             dropdown.css({
                 opacity: 0,
                 "pointer-events": "none"
             })
-        }, 100)
+        })
+        dropdown.append(itemElement)
+    });
+
+    dropdown[0].scrollTo({top: 0, behavior: 'auto'});
+    // Logic for positioning the dropdown menu.
+    let x = element.offset().left + element.width() / 2 - (dropdown.width() / 2)
+    let y = element.offset().top + element.height() + 40
+    if (y + dropdown.height() > window.innerHeight) {
+        y = window.innerHeight - dropdown.height() - 10
     }
+    if (x + dropdown.width() > window.innerWidth) {
+        x = window.innerWidth - dropdown.width() - 10
+    }
+    // Apply CSS styles to position the dropdown correctly
+    dropdown.css({
+        top: y,
+        left: x,
+        opacity: 1,
+        "pointer-events": "all"
+    })
+    dropdown.focus();
 }
+
+dropdown.on("blur", () => {
+    dropdown.css({
+        opacity: 0,
+        "pointer-events": "none"
+    })
+});
