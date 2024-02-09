be8b916c7cd55d81842d391cd774fb6fddd7a09e
drew-chase <dcmanproductions@gmail.com>
2024-02-09 13:40:56 -0500

    Update API URL and refactor list item generation in database-list.js
    The API endpoint for fetching locations has been updated to a new URL, and the code for creating the list items has been refactored. The old List class usage has been replaced by jQuery for creating list items and handling button actions. Individual edit and more buttons are now added to each item.

--- src/assets/js/database-list.js
+++ src/assets/js/database-list.js
@@ -1,5 +1,3 @@
-import List from './element-list.js';
-
 const list = $(".list");
 loadList();
 
@@ -12,33 +10,41 @@ async function loadList(id = "") {
 
 
     loadLabels();
-    loadDropdown();
 }
 
 
 async function loadDatabaseList() {
     list.html("");
 
-    const url = "http://pricing.local/api/locations/";
+    const url = "https://pricing-new.mardens.com/api/locations/";
     try {
         const json = await $.ajax({url: url, method: "GET"});
         console.log(json)
         const items = json.items;
         for (let i = 0; i < items.length; i++) {
-            let item = new List(`https://pricing.mardens.com/icons/${items[i].image}`, items[i].name, true, [
-                {
-                    name: "Edit",
-                    action: () => {
-                        console.log("Edit");
+
+            let item = $(`
+                        <div class="list-item">
+                            <img src="${items[i]["0"]["image"]}" alt="">
+                            <span class="title">${items[i]["name"]}</span>
+                        </div> `);
+            const editButton = $(`<button class="edit-list-button" title="Edit product"><img src="assets/images/icons/edit.svg" alt=""></button>`);
+            const moreButton = $(`<button class="more-options" data-title="More Options" tabindex="0"><img src="assets/images/icons/more.svg" alt=""></button>`);
+
+            moreButton.on('click', (e)=> {
+                openDropdown(moreButton, {
+                    "Edit": () => {
+                        console.log("Edit")
+                    },
+                    "Delete": () => {
+                        console.log("Delete")
                     }
-                },
-                {
-                    name: "Delete",
-                    action: () => {
-                        console.log("Delete");
-                    }
-                }
-            ]);
+                })
+            });
+
+            item.append(editButton);
+            item.append(moreButton);
+
             list.append(item);
         }
     } catch (e) {
