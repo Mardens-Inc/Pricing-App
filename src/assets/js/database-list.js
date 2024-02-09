fc0c24488cd1986f7813d0e16b103af4dfd45502
drew-chase <dcmanproductions@gmail.com>
2024-02-09 15:55:31 -0500

    Add search functionality to database list
    The database list now includes the ability to search and filter results. Searching is achieved through an async function that fetches the data based on the user's query. Additionally, changes were made to the database list loading method to accommodate pagination and display relevant data in a better format.

--- src/assets/js/database-list.js
+++ src/assets/js/database-list.js
@@ -1,52 +1,78 @@
 const list = $(".list");
+let loadedListItems = [];
+let currentId = "";
 loadList();
 
-async function loadList(id = "") {
+async function loadList(id = "", query = "") {
     if (!id) {
-        await loadDatabaseList();
+        await loadDatabaseList(query);
     } else {
-
+        currentId = id;
     }
-
 
     loadLabels();
 }
 
 
-async function loadDatabaseList() {
+$("#search").on("keyup", async (e) => {
+    await loadList(currentId, e.target.value);
+});
+
+
+async function loadDatabaseList(query = "") {
     list.html("");
 
-    const url = "https://pricing-new.mardens.com/api/locations/";
+    const url = "https://pricing-new.mardens.com/api/locations/all";
     try {
-        const json = await $.ajax({url: url, method: "GET"});
-        console.log(json)
-        const items = json.items;
-        for (let i = 0; i < items.length; i++) {
-
-            let item = $(`
-                        <div class="list-item">
-                            <img src="${items[i]["0"]["image"]}" alt="">
-                            <span class="title">${items[i]["name"]}</span>
-                        </div> `);
-            const editButton = $(`<button class="edit-list-button" title="Edit product"><img src="assets/images/icons/edit.svg" alt=""></button>`);
-            const moreButton = $(`<button class="more-options" data-title="More Options" tabindex="0"><img src="assets/images/icons/more.svg" alt=""></button>`);
-
-            moreButton.on('click', (e)=> {
-                openDropdown(moreButton, {
-                    "Edit": () => {
-                        console.log("Edit")
-                    },
-                    "Delete": () => {
-                        console.log("Delete")
-                    }
-                })
+        loadedListItems = await $.ajax({url: url, method: "GET"});
+        if (query !== "") {
+            loadedListItems = loadedListItems.filter((item) => {
+                return item["name"].toLowerCase().includes(query.toLowerCase()) || item["location"].toLowerCase().includes(query.toLowerCase()) || item["po"].toLowerCase().includes(query.toLowerCase());
             });
-
-            item.append(editButton);
-            item.append(moreButton);
-
-            list.append(item);
         }
+        $(".pagination").pagination({
+            dataSource: loadedListItems,
+            autoHideNext: false,
+            autoHidePrevious: false,
+            pageSize: 10,
+            pageRange: 1,
+
+            callback: (data, pagination) => {
+                console.log(data, pagination);
+
+                list.html("");
+                data.forEach((item) => {
+
+
+                    let i = $(`
+                        <div class="list-item">
+                            <img src="${item["image"]===""?"/assets/images/icon.png":item["image"]}" alt="">
+                            <span class="title">${item["name"]}<span class="extra">${item["location"]} - ${item["po"]}</span></span>
+                            
+                        </div> `);
+                    const editButton = $(`<button class="edit-list-button" title="Edit product"><img src="assets/images/icons/edit.svg" alt=""></button>`);
+                    const moreButton = $(`<button class="more-options" data-title="More Options" tabindex="0"><img src="assets/images/icons/more.svg" alt=""></button>`);
+
+                    moreButton.on('click', (e) => {
+                        openDropdown(moreButton, {
+                            "Edit": () => {
+                                console.log("Edit")
+                            },
+                            "Delete": () => {
+                                console.log("Delete")
+                            }
+                        })
+                    });
+
+                    i.append(editButton);
+                    i.append(moreButton);
+
+                    list.append(i);
+                });
+                loadLabels();
+
+            }
+        })
     } catch (e) {
         console.error("Unable to fetch data from the server\n", url, e);
     }
