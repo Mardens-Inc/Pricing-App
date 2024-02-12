5782a09c89dc2cc8b82e0d45906653d8b65ae099
drew-chase <dcmanproductions@gmail.com>
2024-02-12 12:07:04 -0500

    Implement DatabaseList class in database-list.js
    A new class, DatabaseList, has been added to the database-list.js file which significantly refactors the code. This class includes methods for fetching and building a list of database items. Redundant code and comments have been commented out, thus improving the overall efficiency.

--- src/assets/js/database-list.js
+++ src/assets/js/database-list.js
@@ -1,8 +1,70 @@
+export default class DatabaseList {
+    constructor(id) {
+        this.list = $(".list");
+        this.items = [];
+        this.id = id;
+        this.getListHeader().then((name, location, po, image, options, posted) => {
+            if (image !== "") {
+                img.attr('src', image);
+                img.css("border-radius", "12px");
+            }
+            title.html(name);
+            subtitle.html(`${location} - ${po}`).css("display", "");
+            backButton.css("display", "");
+            this.list.html("");
+            $(".pagination").html("");
+            $("#search").val("");
+        });
+        this.loadView("", true);
+
+    }
+
+    async loadView(query, force = false) {
+
+        const newList = await this.getListItems(query);
+        if (force || newList !== this.items) {
+            this.items = newList;
+            this.buildList()
+        }
+    }
+
+    async getListItems(query = "") {
+        const url = `https://pricing-new.mardens.com/api/locations/${this.id}`;
+        let newList = await $.ajax({url: url, method: "GET"});
+        console.log(newList)
+        if (query !== "") {
+            newList = newList.filter((item) => item["name"].toLowerCase().includes(query.toLowerCase()));
+        }
+        return newList;
+    }
+
+    buildList() {
+        this.list.html("");
+        this.items.forEach((item) => {
+            const listItem = $("<div>").addClass("list-item");
+            const name = $("<h3>").html(item["name"]);
+            const location = $("<p>").html(item["location"]);
+            listItem.append(name, location);
+            this.list.append(listItem);
+        });
+    }
+
+    async getListHeader() {
+        const url = `https://pricing-new.mardens.com/api/locations/${this.id}`;
+        const json = await $.ajax({url: url, method: "GET"});
+        return {name: json["name"], location: json["location"], po: json["po"], image: json["image"], options: json["options"], posted: json["posted_date"]};
+    }
+}
+
+
+/*
 const list = $(".list");
 let loadedListItems = [];
 let currentId = "";
 let currentQuery = "";
+backButton.on('click', () => loadView("", "", true));
 loadView("", "", true);
+setInterval(async () => await loadView(currentId, currentQuery), 30 * 1000);
 
 async function loadList(id = "", query = "") {
     currentQuery = query;
@@ -10,6 +72,7 @@ async function loadList(id = "", query = "") {
     if (!id) {
         return await loadDatabaseList(query);
     } else {
+        return await loadDatabase(id, query);
     }
 
 }
@@ -20,106 +83,26 @@ $("#search").on("keyup", async (e) => {
 });
 
 
-async function loadDatabaseList(query = "") {
+async function loadDatabase(id = "", query = "") {
+    const {_, name, location, po, image, options, posted} = await getListHeader(id);
+    console.log(id, name, location, po, image, options, posted);
 
-    const url = "https://pricing-new.mardens.com/api/locations/all";
-    try {
-        let newList = await $.ajax({url: url, method: "GET"});
-        if (query !== "") {
-            newList = newList.filter((item) => {
-                return item["name"].toLowerCase().includes(query.toLowerCase()) || item["location"].toLowerCase().includes(query.toLowerCase()) || item["po"].toLowerCase().includes(query.toLowerCase());
-            });
-        }
-        return newList;
-    } catch (e) {
-        console.error("Unable to fetch data from the server\n", url, e);
+    if (image !== "") {
+        img.attr('src', image);
+        img.css("border-radius", "12px");
     }
-}
+    title.html(name);
+    subtitle.html(`${location} - ${po}`).css("display", "");
+    backButton.css("display", "");
+    list.html("");
+    $(".pagination").html("");
+    $("#search").val("");
 
-function buildPagination() {
-    try {
+    loadedListItems = [];
 
-        $(".pagination").pagination({
-            dataSource: loadedListItems,
-            autoHideNext: false,
-            autoHidePrevious: false,
-            pageSize: 10,
-            pageRange: 1,
-
-            callback: (data, pagination) => {
-                list.html("");
-                data.forEach((item) => {
-
-
-                    let i = $(`
-                        <div class="list-item">
-                            <img src="${item["image"] === "" ? "/assets/images/icon.png" : item["image"]}" alt="">
-                            <span class="title">${item["name"]}<span class="extra">${item["location"]} - ${item["po"]}</span></span>
-                            
-                        </div> `);
-                    const editButton = $(`<button class="edit-list-button" title="Edit product"><img src="assets/images/icons/edit.svg" alt=""></button>`);
-                    const moreButton = $(`<button class="more-options" data-title="More Options" tabindex="0"><img src="assets/images/icons/more.svg" alt=""></button>`);
-
-                    moreButton.on('click', (e) => {
-                        openDropdown(moreButton, {
-                            "Edit": () => {
-                                console.log("Edit")
-                            },
-                            "Delete": () => {
-                                console.log("Delete")
-                            }
-                        })
-                    });
-
-                    i.append(editButton);
-                    i.append(moreButton);
-
-                    list.append(i);
-                });
-                loadLabels();
-            }
-        })
-    } catch (e) {
-        console.error(`Unable to build pagination\nLoaded List: ${loadedListItems}\n`, e);
-    }
-}
-
-const refreshTimer = setInterval(async () => await loadView(currentId, currentQuery), 30 * 1000);
-
-async function loadView(id, query, force = false) {
-    const newList = await loadList(id, query);
-    if (force || newList !== loadedListItems) {
-        loadedListItems = newList;
-        buildPagination();
-    }
+    return [];
 }
 
 
-import Voice from "./Voice.js";
 
-$("#voice-search-button").on('click', () => {
-
-    let voice = new Voice();
-    if (voice.unsupported) {
-        alert(`Your browser does not support voice recognition`);
-        return;
-    }
-    const button = $("#voice-search-button");
-    if (button.hasClass("primary")) {
-        button.removeClass("primary");
-        voice.stop();
-        return;
-    }
-    button.addClass("primary");
-    $(voice).on("interim", async (event, transcript) => {
-        console.log("Interim: " + transcript);
-        $("#search").val(transcript);
-        await loadView(currentId, transcript);
-    });
-    $(voice).on("result", async (event, transcript) => {
-        button.removeClass("primary");
-        voice.stop();
-    });
-
-    voice.start();
-})
+ */
