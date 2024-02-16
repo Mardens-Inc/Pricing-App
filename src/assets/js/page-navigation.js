c64a09e8788b457d816e24299b7f976da8f1b53f
drew.chase <dcmanproductions@gmail.com>
2024-02-16 14:41:09 -0500

    Implement localStorage for loaded database
    A loaded database is now stored and retrieved from the window's localStorage. This was achieved by changing logic flow for the display of the edit button, adding listeners for loadExternalView, unloadExternalView, and a search event. The event handlers also make use of the localStorage to store or remove the loadedDatabase.

--- src/assets/js/page-navigation.js
+++ src/assets/js/page-navigation.js
@@ -2,7 +2,30 @@ import DatabaseList from "./database-list.js";
 import DirectoryList from "./directory-list.js";
 
 
+$("#edit-button").css('display', 'none');
 const directory = new DirectoryList();
+let database = null;
+if(window.localStorage.getItem("loadedDatabase") !== null){
+    database = new DatabaseList(window.localStorage.getItem("loadedDatabase"));
+    $("#edit-button").css('display', "");
+}else{
+    directory.loadView("", true);
+}
 $(directory).on("loadExternalView", (event, id) => {
-    const database = new DatabaseList(id);
+    database = new DatabaseList(id);
+    window.localStorage.setItem("loadedDatabase", id);
+    $("#edit-button").css('display', "");
+});
+$(directory).on("unloadExternalView", (event, id) => {
+    database = null;
+    $("#edit-button").css('display', 'none');
+    window.localStorage.removeItem("loadedDatabase");
+});
+$(document).on("search", async (event, data) => {
+    let results = [];
+    if (database !== null) {
+        results = await database.search(data);
+    } else {
+        results = await directory.search(data);
+    }
 });
