fbd10b6a4a5716c2ef5daac2abfc0b4561237c71
drew.chase <dcmanproductions@gmail.com>
2024-02-16 14:40:37 -0500

    Add search function and Filemaker import form
    Included a new search function to refine the items within the database list. Also built a form for importing databases from Filemaker. Adjustments were made to ensure compatibility of the updated url scheme. Additionally, condition for list building was amended to facilitate display of the new import form when item list is empty.

--- src/assets/js/database-list.js
+++ src/assets/js/database-list.js
@@ -1,3 +1,5 @@
+import Filemaker from "https://cdn.jsdelivr.net/gh/Mardens-Inc/Filemaker-API/js/Filemaker.js";
+
 export default class DatabaseList {
     constructor(id) {
         this.list = $(".list");
@@ -16,7 +18,6 @@ export default class DatabaseList {
             $("#search").val("");
         });
         this.loadView("", true);
-
     }
 
     async loadView(query, force = false) {
@@ -28,10 +29,15 @@ export default class DatabaseList {
         }
     }
 
+    async search(query) {
+        await this.loadView(query, true);
+        return this.items;
+    }
+
     async getListItems(query = "") {
-        const url = `https://pricing-new.mardens.com/api/locations/${this.id}`;
+        const url = `${baseURL}/api/location/${this.id}/`;
         let newList = await $.ajax({url: url, method: "GET"});
-        console.log(newList)
+        newList = newList["results"]["items"];
         if (query !== "") {
             newList = newList.filter((item) => item["name"].toLowerCase().includes(query.toLowerCase()));
         }
@@ -39,6 +45,10 @@ export default class DatabaseList {
     }
 
     buildList() {
+        if (this.items.length === 0) {
+            this.buildImportFilemakerForm()
+            return;
+        }
         this.list.html("");
         this.items.forEach((item) => {
             const listItem = $("<div>").addClass("list-item");
@@ -49,8 +59,43 @@ export default class DatabaseList {
         });
     }
 
+    async buildImportFilemakerForm() {
+        const html = $(await $.ajax({url: "assets/html/import-filemaker-form.html", method: "GET"}));
+        const filemaker = new Filemaker("https://lib.mardens.com/fmutil", "admin", "19MRCC77!");
+        const databases = await filemaker.getDatabases();
+        for (const database of databases) {
+            const item = $(`
+                <input type="radio" id="filemaker-database-${database}" name="filemaker-database" value="${database}">
+                <label for="filemaker-database-${database}" class="list-item">${database.replace(/-/g, " ")}</label>
+            `)
+            html.find("#filemaker-databases .list").append(item);
+        }
+        html.find("#filemaker-databases input").on('change', async (e) => {
+            html.find("#filemaker-databases button[type='submit']").prop("disabled", false);
+        });
+        html.find("#filemaker-databases").on('submit', async (e) => {
+            const database = html.find('input[name="filemaker-database"]:checked').val();
+            if (database === undefined) {
+                console.error("No database selected");
+                return;
+            }
+        });
+
+        html.find("#filemaker-credentials").on("submit", async (e) => {
+            const username = html.find("#filemaker-username").val();
+            const password = html.find("#filemaker-password").val();
+        });
+
+
+        this.list.html(html);
+    }
+
+    buildItemizedList() {
+    }
+
+
     async getListHeader() {
-        const url = `https://pricing-new.mardens.com/api/locations/${this.id}`;
+        const url = `${baseURL}/api/location/${this.id}/?headings=true`;
         const json = await $.ajax({url: url, method: "GET"});
         return {name: json["name"], location: json["location"], po: json["po"], image: json["image"], options: json["options"], posted: json["posted_date"]};
     }
