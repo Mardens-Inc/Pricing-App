3f7b925249fd9f404ff1369b126f22e3fc1f67b4
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-02-26 14:53:27 -0500

    Refactor database options and icon list manipulation
    The database options manager has been updated to include an improved structure and functionality. A new `buildOptionsForm` function has been introduced to construct the form for given options. The former `navigateToIconList` function has been refactored to `buildIconList` to enhance clarity. Moreover, a `setDefaultOptionValues` function has been added to set the default values for the user's form.

--- src/assets/js/database-options-manager.js
+++ src/assets/js/database-options-manager.js
@@ -36,15 +36,25 @@
  */
 
 
+import {download} from "./filesystem.js";
+
 /**+
  * @type {ListHeading}
  */
 let currentOptions = {};
 
+/**
+ * Builds the options form for a given id.
+ *
+ * @param {string} id - The id to retrieve the options for.
+ * @return {Promise<JQuery>} - A Promise that resolves to the HTML form representing the options.
+ */
 async function buildOptionsForm(id) {
     const html = $(await $.ajax({url: "assets/html/database-options-form.html", method: "GET"}));
     currentOptions = await getCurrentOptions(id);
-    await navigateToIconList(html);
+    await buildIconList(html);
+    setDefaultOptionValues(html);
+
     return html;
 }
 
@@ -56,7 +66,13 @@ async function getCurrentOptions(id) {
     return $.get(`${baseURL}/api/location/${id}/?headings=true`);
 }
 
-async function navigateToIconList(html) {
+/**
+ * Navigates to the icon list and populates it with icons.
+ *
+ * @param {JQuery} html - The HTML element or DOM object where the icons will be appended.
+ * @return {Promise} - A promise that resolves when the icons have been loaded and rendered.
+ */
+async function buildIconList(html) {
     /**
      * @type {Icon[]}
      */
@@ -80,7 +96,19 @@ async function navigateToIconList(html) {
         html.find("#icons.list").prepend(html.find(`input[value="${selectedIcon.name}"]`).parent());
         html.find("#icons.list").prepend(html.find(`label[for="${selectedIcon.name}"]`).parent());
     }
+}
 
+/**
+ * Sets the default option values for the form.
+ * @param {JQuery} html
+ */
+function setDefaultOptionValues(html) {
+    html.find("input#database-name").val(currentOptions.name);
+    html.find("input#database-location").val(currentOptions.location);
+    html.find("input#database-po").val(currentOptions.po);
+    html.find("toggle#show-date").prop("checked", currentOptions.options["show-date"]);
+    html.find("toggle#voice-search").prop("checked", currentOptions.options["voice-search"]);
+    html.find("toggle#print").prop("checked", currentOptions.options["print"]);
 
 }
 
