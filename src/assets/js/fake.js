76629a8ad141b7bb22a31dbb5310519d621ce24c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-07 15:03:19 -0500

    Refactor object property definitions in fake.js
    Changes are made to property definitions in the fake.js file to simplify them. The 'VoiceSearchOptions' object was removed and replaced with a single Boolean property 'voice-search'. Several properties were consolidated in the 'PrintOptions' and 'RecordPrintData' objects, focusing on the essential dataset attributes, which improves overall code readability and maintainability.

--- src/assets/js/fake.js
+++ src/assets/js/fake.js
@@ -13,7 +13,7 @@
  * @property {bool} allow-inventorying
  * @property {bool} allow-additions
  * @property {PrintOptions} print-form
- * @property {VoiceSearchOptions} voice-form
+ * @property {boolean} voice-search
  * @property {Object[]} columns
  * @property {string} columns.name
  * @property {boolean} columns.visible
@@ -24,24 +24,28 @@
  *
  * @typedef {Object} PrintOptions
  * @property {boolean} enabled
- * @property {string} label-name
+ * @property {string} label
  * @property {string} year
- * @property {string} price-column
- * @property {string} retail-price-column
+ * @property {string} size
  * @property {boolean} show-retail
- * @property {string} label-size
  */
 
-/**
- * @typedef {Object} VoiceSearchOptions
- * @property {boolean} enabled
- * @property {string} voice-description-column
- * @property {string} voice-price-column
- */
 
 /**
  * @typedef {Object} Icon
  * @property {string} name
  * @property {string} url
  * @property {string} file
+ */
+
+/**
+ * @typedef {Object} RecordPrintData
+ * @property {string} label
+ * @property {string} year
+ * @property {string} size
+ * @property {string?} retail
+ * @property {string?} mp
+ * @property {string?} department
+ *
+ *
  */
