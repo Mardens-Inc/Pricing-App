84ee5765f4d8cbfeb3c3e7d018c20b88230bcfc1
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-07 15:02:31 -0500

    Improve data printing in crossplatform utility
    Enhanced the print function in the crossplatform-utility.js file to support printing multiple data points, including department, label, retail, mp, and year. This amendment also includes the use of a CSS file specifically for styling the printed output.

--- src/assets/js/crossplatform-utility.js
+++ src/assets/js/crossplatform-utility.js
@@ -2,12 +2,39 @@ const isDedicatedClient = window.__TAURI__;
 
 /**
  * Print data to a printer
- * @param {PrintOptions} data
+ * @param {RecordPrintData} data
  */
 function print(data) {
+    console.log(data);
     const printWindow = window.open('', 'PRINT', 'height=400,width=600');
-    const page = $(`<html lang="en"><head><title>${print}</title></head></html>`);
 
+    let bodyHtml = "";
+    if (data.department) {
+        bodyHtml += `<p class="dpt">${data.department}</p>`;
+    }
+    bodyHtml += `<p class="label">${data.label}</p>`;
+    if (data.retail) {
+        bodyHtml += `<p class="rp">${data.retail}</p>`;
+    }
+    if (data.mp) {
+        bodyHtml += `<p class="mp">${data.mp}</p>`;
+    }
+    bodyHtml += `<p class="year">${data.year}</p>`;
+
+    const pageHtml = `<html lang="en">
+                        <head>
+                            <title>Print</title>
+                            <link rel="stylesheet" href="assets/css/printer.css">
+                        </head>
+                        <body size="${data.size}">
+                            ${bodyHtml}
+                        </body>
+                    </html>`;
+
+    console.log(pageHtml);
+    printWindow.document.write(pageHtml);
+    printWindow.print();
+    printWindow.close();
 }
 
 
