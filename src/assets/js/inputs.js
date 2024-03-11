be6c6989df852d2bf1c7a3c330f211611d56faa5
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-11 12:00:12 -0400

    Implement debounce to throttle toggle events
    This commit introduces a debounce behavior for the 'toggle' event handlers in 'inputs.js'. The event listeners for the 'click' and 'toggle' events on the toggle class elements are now wrapped in a setTimeout with a 500 milliseconds delay, reducing the rate at which the handler executes and improving performance. The addition of 'debounce' also ensures old event handlers are cleared out to prevent memory leaks or unexpected behavior.

--- src/assets/js/inputs.js
+++ src/assets/js/inputs.js
@@ -1,4 +1,6 @@
+let debounce = 0;
 $(document).on("load", () => {
+    clearTimeout(debounce);
     $(`[toggle-hidden]`).each((_, element) => {
         let target = $(element);
         let toggle = $(`#${target.attr("toggle-hidden")}`);
@@ -8,34 +10,34 @@ $(document).on("load", () => {
             target.hide();
         }
     });
-    const toggles = $("toggle");
+    debounce = setTimeout(() => {
+        const toggles = $("toggle");
 
-    toggles.off("click"); // Remove any existing click event listeners.
-    // Add a click event listener to all elements with the class "toggle".
-    toggles.on("click", (e) => {
-        // Prevent the default click behavior.
-        e.preventDefault();
-        // Get the target of the click event.
-        let target = $(e.target);
-        // Get the current value of the "value" attribute of the target.
-        let value = target.attr("value") === "true";
-        // Set the "value" attribute of the target to the opposite of its current value.
-        target.attr("value", `${!value}`);
-        // Trigger a "toggle" event on the target with the new value.
-        target.trigger("toggle", [{value: !value}]);
+        // Add a click event listener to all elements with the class "toggle".
+        toggles.on("click", (e) => {
+            // Prevent the default click behavior.
+            e.preventDefault();
+            // Get the target of the click event.
+            let target = $(e.target);
+            // Get the current value of the "value" attribute of the target.
+            let value = target.attr("value") === "true";
+            // Set the "value" attribute of the target to the opposite of its current value.
+            target.attr("value", `${!value}`);
+            // Trigger a "toggle" event on the target with the new value.
+            target.trigger("toggle", [{value: !value}]);
 
-    });
+        });
 
-    toggles.off("toggle"); // Remove any existing "toggle" event listeners.
-    toggles.on("toggle", (e, data) => {
-        let target = $(e.target);
-        console.log(target.attr("id"));
-        // Toggles the visibility of the target element and the element with the id specified in the "toggle-hidden" attribute of the target.
-        $(`[toggle-hidden="${target.attr("id")}"]`).toggle();
-        // set the value of the target element to the value of the data object.
-        target.attr('value', data.value);
-    });
-
+        // toggles.off("toggle"); // Remove any existing "toggle" event listeners.
+        toggles.on("toggle", (e, data) => {
+            let target = $(e.target);
+            // Toggles the visibility of the target element and the element with the id specified in the "toggle-hidden" attribute of the target.
+            $(`[toggle-hidden="${target.attr("id")}"]`).toggle();
+            // set the value of the target element to the value of the data object.
+            target.attr('value', data.value);
+            target.data("events", "toggle")
+        });
+    }, 500);
 
     /**
      * Represents a draggable and droppable area.
