667aea0962b2754bed6a9d01f36f793b40851197
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-12 16:29:07 -0400

    Update Authentication API and improve user experience
    This commit updates the reference to the latest version of the Authentication API. Additionally, it augments the visibility of error messages in the login form and tweaks the behavior after logging in. Specifically, the "loginPrompt" is removed from local storage and non-admin users are prevented from accessing certain resources. It also cleans up some unnecessary commented out code.

--- src/assets/js/authentication.js
+++ src/assets/js/authentication.js
@@ -1,4 +1,4 @@
-import Authentication from 'https://cdn.jsdelivr.net/gh/Mardens-Inc/Authentication-API@240fe69e848dbc2eb4c3c17f510a9ba85c68cb69/js/authentication.js';
+import Authentication from 'https://cdn.jsdelivr.net/gh/Mardens-Inc/Authentication-API@3b8d66f2d6555cacafd8fa30a62fb19ac2e34442/js/authentication.js';
 import {alert, closePopup, openPopup} from "./popups.js";
 
 const auth = new Authentication();
@@ -21,7 +21,7 @@ loginButton.on('click', async () => {
                 closePopup("login");
                 window.location.reload();
             } else {
-                loginForm.find('.error').text(response['message']);
+                loginForm.find('.error').css('display', "").text(response['message']);
             }
         });
     }
@@ -32,13 +32,19 @@ $(auth).on("log-out", () => {
     loginButton.attr('data-title', "Login");
     $("[authorized-access]").remove();
     console.log('logged out')
+    window.localStorage.removeItem("loginPrompt");
 });
 
 $(auth).on("logged-in", async () => {
     loginButton.find("img").attr("src", "assets/images/icons/logout.svg");
     loginButton.attr('data-title', "Logout");
     console.log('logged in')
+
+    if (!auth.getUserProfile().admin) {
+        $("[authorized-access]").remove();
+    }
 });
+
 
 (async () => {
     try {
@@ -46,7 +52,6 @@ $(auth).on("logged-in", async () => {
         if (typeof response !== 'object') {
             $(auth).trigger('log-out');
         }
-        // $("#new-button").trigger("click");
     } catch (e) {
         $(auth).trigger('log-out');
     }
