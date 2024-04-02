54b6d844546626f4f2bf48bdec958376a9a813e4
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-02 15:06:29 -0400

    Refactor update functionality in updater.rs
    Changes involve updating the way versions are fetched, removing outdated update downloading logic and adding new functions to handle update downloads. Also, the `get_current_version` and `download_update` functions are now tauri commands. The `install_update` function has been amended to utilize a downloaded updater instead of copying the process.

--- src-tauri/src/updater.rs
+++ src-tauri/src/updater.rs
@@ -1,79 +1,80 @@
-use std::collections::HashMap;
-use std::io::prelude::*;
 use std::io::Write;
+use std::path::PathBuf;
 
-struct Version {
-    version: String,
-    changelog: String,
+use tauri::command;
+
+#[tauri::command]
+pub fn get_current_version() -> String {
+    return env!("CARGO_PKG_VERSION").to_string();
 }
 
-async fn get_versions() -> Vec<Version> {
-    let url = "https://pricing-new.mardens.com/clients/versions";
-    let mut versions = Vec::new();
+
+async fn get_versions() -> Vec<String> {
+    let url = "https://pricing-new.mardens.com/api/clients/versions";
     if let Ok(body) = reqwest::get(url).await.unwrap().text().await {
-        let json = serde_json::from_str::<HashMap<String, String>>(&body).unwrap();
-        for (version, changelog) in json.iter() {
-            versions.push(Version {
-                version: version.to_string(),
-                changelog: changelog.to_string(),
-            });
-        }
+        let versions = serde_json::from_str::<Vec<String>>(&body).unwrap();
+        return versions;
     }
-
-
-    return versions;
+    return Vec::new();
 }
 
 async fn check_version_for_updates() -> bool {
+    println!("Checking for updates");
     let current_version = env!("CARGO_PKG_VERSION");
     let versions = get_versions().await;
-    let latest_version = versions.first().unwrap();
-
-    return latest_version.version != current_version;
+    if let Some(latest_version) = versions.last() {
+        return latest_version != current_version;
+    }
+    return false;
 }
 
-async fn download_update() {
-    // get prerequisite data
-    let versions = get_versions().await;
-    let latest_version = versions.first().unwrap();
-    let operating_system = std::env::consts::OS;
+#[tauri::command]
+pub async fn download_update() -> bool {
+    if !check_version_for_updates().await {
+        return false;
+    }
+    println!("Update found!");
+
+    if !PathBuf::from("update.exe").exists() {
+        if !download_updater().await {
+            return false;
+        }
+    }
 
     // build download url
-    let url = format!("https://pricing-new.mardens.com/clients/update/{}/{}", operating_system, latest_version.version);
-
-    // build download path
-    let tmp_dir = std::env::temp_dir().join("mardens_pricing").join("update").join(&latest_version.version);
-    let tmp_file = tmp_dir.join("update.zip");
-    let tmp_dir = tmp_dir.to_str().unwrap();
-    let tmp_file = tmp_file.to_str().unwrap();
-
-    // create directory
-    std::fs::create_dir_all(tmp_dir).unwrap();
-
-    // download file
-    if let Ok(response) = reqwest::get(&url).await {
-        let mut file = std::fs::File::create(tmp_file).unwrap();
+    println!("Downloading update...");
+    let url = "https://pricing-new.mardens.com/api/clients/latest";
+    if let Ok(response) = reqwest::get(url).await {
+        let mut file = std::fs::File::create("tmp.exe").unwrap();
         let content = response.bytes().await.unwrap();
         file.write_all(&content).unwrap();
-
-        // unzip file
-        let mut archive = zip::ZipArchive::new(std::fs::File::open(tmp_file).unwrap()).unwrap();
-        archive.extract(tmp_dir).unwrap();
-
-        // delete zip file
-        std::fs::remove_file(tmp_file).unwrap();
+        println!("Finished downloading update!");
+        return true;
     }
+
+    return false;
 }
 
-async fn install_update() {
-    let current_exe = std::env::current_exe().unwrap();
-    let tmp_dir = std::env::temp_dir().join("mardens_pricing").join("update").join(env!("CARGO_PKG_VERSION"));
-    let tmp_exe = tmp_dir.join("Pricing App");
+async fn download_updater() -> bool
+{
+    println!("Downloading updater...");
+    let url = "https://pricing-new.mardens.com/api/clients/updater";
+    if let Ok(response) = reqwest::get(url).await {
+        let mut file = std::fs::File::create("updater.exe").unwrap();
+        if let Ok(content) = response.bytes().await {
+            file.write_all(&content).unwrap();
+            println!("Finished downloading updater!");
+            return true;
+        }
+    }
+    return false;
+}
 
-    // copy new exe
-    // std::fs::copy(tmp_exe, current_exe).unwrap();
-    //
-    // // restart app
-    // std::process::Command::new(current_exe).spawn().unwrap();
-    // std::process::exit(0);
+#[command]
+pub fn install_update() -> bool {
+    if PathBuf::from("tmp.exe").exists() {
+        std::process::Command::new("updater.exe").args(&["tmp.exe", "pricing-app.exe"]).spawn().unwrap();
+        return true;
+    }
+    return false;
 }
