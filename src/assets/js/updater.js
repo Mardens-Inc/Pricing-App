45cf9f623c7e7581820e9730493caf8d4ac486ee
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-02 15:07:44 -0400

    Add update checker functionality to the application
    This update includes the addition of a new JS file named updater.js. The new file adds capability to automatically check for updates if the app is running as a dedicated client. It uses Tauri features to perform the update check and to retrieve the current version number.

---
+++ src/assets/js/updater.js
@@ -0,0 +1,54 @@
+import {isDedicatedClient} from "./crossplatform-utility.js";
+import {alert} from "./popups.js";
+
+window.canUpdate = false;
+
+if (isDedicatedClient) {
+    getCurrentVersion().then(version => {
+        window.version = version;
+    })
+} else {
+    window.version = "web-client";
+}
+
+/**
+ * Checks for updates.
+ *
+ * @returns {Promise<boolean>} A Promise that resolves to a boolean if an update is available or not.
+ */
+async function checkForUpdates() {
+    if (isDedicatedClient) {
+        const result = await window.__TAURI__.invoke('download_update');
+        console.log(`Checked for update: ${result ? "Update available" : "No update available"}`);
+        return result;
+    }
+}
+
+/**
+ * Retrieves the current version from the Tauri application.
+ *
+ * @returns {Promise<string>} A Promise that resolves to the current version.
+ */
+async function getCurrentVersion() {
+    if (isDedicatedClient)
+        return await window.__TAURI__.invoke('get_current_version');
+}
+
+if (isDedicatedClient) {
+    let update = setInterval(async () => check, 1000 * 60 * 15); // Check every 15 minutes
+    const check = (async () => {
+        if (await checkForUpdates()) {
+            $(window).trigger("updateAvailable")
+            window.canUpdate = true;
+            clearInterval(update);
+            alert("An update is available. Please restart the application to apply the update.", () => {
+                window.__TAURI__.invoke('install_update');
+            })
+        }
+    });
+    check();
+}
+
+window.checkForUpdates = checkForUpdates;
+
+export {checkForUpdates, getCurrentVersion}
