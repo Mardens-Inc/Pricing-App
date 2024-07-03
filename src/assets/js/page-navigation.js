29f096cd29024290b59eb1c60f2961df3356354e
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-03 09:19:48 -0400

    Refactor page-navigation.js for better readability
    Several changes in page-navigation.js have been made to enhance readability. This involves updating the code style to include consistent use of semicolons at end of statements, standardized the usage of quotation marks, and restructured if-else blocks and functions to allow for easier tracing. This commit does not bring any changes to the functionalities of the code.

--- src/assets/js/page-navigation.js
+++ src/assets/js/page-navigation.js
@@ -11,140 +11,176 @@ window.alert = alert;
 window.confirm = confirm;
 
 
-const list = $("main > .list")
-resetListElement()
+const list = $("main > .list");
+resetListElement();
 const editButton = $("#edit-button");
 const newButton = $("#new-button");
 const exportButton = $("#export-button");
 
-if (isDedicatedClient) {
-    getCurrentVersion().then(version => {
-        if (version === "web-client") {
-            subtitle.html(`Web Client`)
-        } else {
-            subtitle.html(`v${version}`)
-        }
-    });
+if (isDedicatedClient)
+{
+    getCurrentVersion().then(version =>
+                             {
+                                 if (version === "web-client")
+                                 {
+                                     subtitle.html(`Web Client`);
+                                 } else
+                                 {
+                                     subtitle.html(`v${version}`);
+                                 }
+                             });
 }
 
 exportButton.hide();
 
-$("#settings-button").on("click", async () => {
+$("#settings-button").on("click", async () =>
+{
     await openSettings();
 });
 
 
-editButton.css('display', 'none');
-exportButton.css('display', 'none');
+editButton.css("display", "none");
+exportButton.css("display", "none");
 
 const directory = new DirectoryList();
 /**
  * @type {DatabaseList|null}
  */
 let database = null;
-$(window).on('load', async () => {
-    try {
-        if (isDedicatedClient) {
+$(window).on("load", async () =>
+{
+    try
+    {
+        if (isDedicatedClient)
+        {
             await loadSettings();
         }
-    } catch (e) {
-        console.error(e)
+    } catch (e)
+    {
+        console.error(e);
     }
-    if (window.localStorage.getItem("loadedDatabase") !== null) {
-        try {
+    if (window.localStorage.getItem("loadedDatabase") !== null)
+    {
+        try
+        {
             database = new DatabaseList(window.localStorage.getItem("loadedDatabase"));
             await database.load();
-            editButton.css('display', "");
-            exportButton.css('display', "");
-            newButton.css('display', 'none');
-        } catch (e) {
-            console.error(e)
-            startLoading({fullscreen: true})
+            editButton.css("display", "");
+            exportButton.css("display", "");
+            newButton.css("display", "none");
+            if (window.localStorage.getItem("page") === "settings")
+            {
+                resetListElement();
+                await database.edit();
+            }
+        } catch (e)
+        {
+            console.error(e);
+            startLoading({fullscreen: true});
             await directory.loadView("", true);
             stopLoading();
         }
-    } else {
-        try {
+    } else
+    {
+        try
+        {
 
-            startLoading({fullscreen: true})
+            startLoading({fullscreen: true});
             await directory.loadView("", true);
             stopLoading();
-        } catch (e) {
-            console.error(e)
+        } catch (e)
+        {
+            console.error(e);
         }
     }
-    setTimeout(() => {
-        askToLogin();
-        $(auth).on('log-out', () => askToLogin())
-    }, 1000);
-})
-$(directory).on("loadExternalView", async (event, id) => {
-    resetListElement()
+    setTimeout(() =>
+               {
+                   askToLogin();
+                   $(auth).on("log-out", () => askToLogin());
+               }, 1000);
+});
+$(directory).on("loadExternalView", async (event, id) =>
+{
+    resetListElement();
     database = new DatabaseList(id);
     await database.load();
     window.localStorage.setItem("loadedDatabase", id);
-    editButton.css('display', "");
-    exportButton.css('display', "");
-    newButton.css('display', 'none');
+    editButton.css("display", "");
+    exportButton.css("display", "");
+    newButton.css("display", "none");
     $(document).trigger("finishedLoadingExternalView", [id]);
 });
-$(directory).on('loadEdit', async (event, id) => {
+$(directory).on("loadEdit", async (event, id) =>
+{
     database = new DatabaseList(id);
     await database.load();
     window.localStorage.setItem("loadedDatabase", id);
-    editButton.css('display', "");
-    exportButton.css('display', "");
-    newButton.css('display', 'none');
-    resetListElement()
+    editButton.css("display", "");
+    exportButton.css("display", "");
+    newButton.css("display", "none");
+    resetListElement();
     await database.edit();
 });
-$(directory).on("unloadExternalView", (event, id) => {
-    resetListElement()
+$(directory).on("unloadExternalView", (event, id) =>
+{
+    resetListElement();
     database = null;
-    editButton.css('display', 'none');
-    exportButton.css('display', "none");
-    newButton.css('display', '');
+    editButton.css("display", "none");
+    exportButton.css("display", "none");
+    newButton.css("display", "");
     window.localStorage.removeItem("loadedDatabase");
 });
 
-$(document).on("search", async (event, data) => {
+$(document).on("search", async (event, data) =>
+{
     // $("#search").val(data);
-    if (database !== null) {
+    if (database !== null)
+    {
         await database.search(data);
-    } else {
-        resetListElement()
+    } else
+    {
+        resetListElement();
         await directory.search(data);
     }
 });
 
-editButton.on("click", async () => {
-    resetListElement()
-    if (database !== null) {
+editButton.on("click", async () =>
+{
+    resetListElement();
+    if (database !== null)
+    {
         await database.edit();
     }
 });
 
-newButton.on("click", async () => {
+newButton.on("click", async () =>
+{
     await DatabaseList.create();
-    resetListElement()
+    resetListElement();
 });
 
-exportButton.on("click", async () => {
-    if (database !== null) {
+exportButton.on("click", async () =>
+{
+    if (database !== null)
+    {
         await database.exportCSV();
     }
 });
 
 
-function resetListElement() {
-    if (list.hasClass("row")) {
-        list.addClass('col')
-        list.removeClass('row')
+function resetListElement()
+{
+    if (list.hasClass("row"))
+    {
+        list.addClass("col");
+        list.removeClass("row");
     }
 }
 
-function askToLogin() {
-    if (!auth.isLoggedIn /*&& window.localStorage.getItem("loginPrompt") === null*/ && isDedicatedClient) {
+function askToLogin()
+{
+    if (!auth.isLoggedIn /*&& window.localStorage.getItem("loginPrompt") === null*/ && isDedicatedClient)
+    {
         // window.localStorage.setItem("loginPrompt", true);
         $("#login-button").trigger("click");
     }
