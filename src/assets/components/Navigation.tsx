b3d4f7516abd17cb594ec9a2b0a3d99a9b5f33da
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-01 15:37:18 -0400

    Revise Navigation component for dark mode and search features
    This update enhances the Navigation component by adding a dark mode toggle and a search bar, improving user experience. The dark mode state is managed with useState and useEffect hooks. Additional style updates and component imports were included to support these new features.

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -1,50 +1,63 @@
-import React from "react";
-import {Link, NavbarMenu, NavbarMenuItem, NavbarMenuToggle} from "@nextui-org/react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
-import ThemeSwitcher from "./ThemeSwitcher.tsx";
+import Logo from "../images/Logo.svg.tsx";
+import {Button, Input, Tooltip} from "@nextui-org/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLock, faMicrophone, faMoon, faSun} from "@fortawesome/free-solid-svg-icons";
+import {applyTheme, getCurrentTheme, Theme} from "../ts/Theme.ts";
+import {useEffect, useState} from "react";
 
 export default function Navigation()
 {
-
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const pages = {
-        "Home": "/",
-        "About": "/about"
-    };
-    const menuItems = Object.keys(pages).map((item, index) =>
+    const [darkMode, setDarkMode] = useState(getCurrentTheme() === Theme.dark);
+    useEffect(() =>
     {
-        const url = Object.values(pages)[index];
-        const isCurrentPage = window.location.pathname === url;
-        return (
-            <NavbarMenuItem key={`${item}-${index}`}>
-                <Link href={url} color={isCurrentPage ? "primary" : "foreground"} aria-current="page" size="lg" className="w-full">
-                    {item}
-                </Link>
-            </NavbarMenuItem>
-        );
-    });
+        applyTheme(darkMode ? Theme.dark : Theme.light);
+    }, [darkMode]);
 
 
     return (
-        <Navbar onMenuOpenChange={setIsMenuOpen}>
+        <Navbar maxWidth={"full"} height={80}>
             <NavbarContent>
-                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden"/>
-                <NavbarBrand>
-                    <p className="font-bold text-inherit">Example</p>
+                <NavbarBrand className={"flex flex-row gap-2"}>
+                    <Logo size={72}/>
+                    <div className={"flex flex-col"}>
+                        <p className="font-bold text-inherit">Pricing Database</p>
+                        <p className="opacity-50 italic">v0.2.3</p>
+                    </div>
                 </NavbarBrand>
             </NavbarContent>
 
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                {menuItems}
+            <NavbarContent className="hidden sm:flex gap-4 w-full" justify="center">
+                <Input
+                    label={"Search"}
+                    placeholder="Search for a database"
+                    radius={"full"}
+                    className={"min-w-md w-1/2 max-w-4xl"}
+                    classNames={{
+                        inputWrapper: "h-12"
+                    }}
+                />
+                <Tooltip content={"Voice Search"}>
+                    <Button radius={"full"} className={"h-12 w-12 aspect-square p-0 min-w-0"}>
+                        <FontAwesomeIcon icon={faMicrophone}/>
+                    </Button>
+                </Tooltip>
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem>
-                    <ThemeSwitcher/>
+                    <Tooltip content={"Login"}>
+                        <Button radius={"full"} className={"h-12 w-12 aspect-square p-0 min-w-0"}>
+                            <FontAwesomeIcon icon={faLock}/>
+                        </Button>
+                    </Tooltip>
+                </NavbarItem>
+                <NavbarItem>
+                    <Tooltip content={`Toggle ${darkMode ? "Light" : "Dark"} Mode`}>
+                        <Button radius={"full"} className={"h-12 w-12 aspect-square p-0 min-w-0"} onClick={() => setDarkMode(prev => !prev)} color={"primary"}>
+                            <FontAwesomeIcon icon={darkMode ? faMoon : faSun}/>
+                        </Button>
+                    </Tooltip>
                 </NavbarItem>
             </NavbarContent>
-
-            <NavbarMenu>
-                {menuItems}
-            </NavbarMenu>
         </Navbar>);
 }
