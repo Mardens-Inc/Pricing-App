673072488ae4a261ed08e5f9a93d5ce32f40f0f1
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-01 15:36:13 -0400

    Add DatabaseItem TypeScript interface
    Introduce a new TypeScript interface `DatabaseItem` and its related sub-interfaces `Column`, `PrintForm`, and `Options`. This addition defines the structure for database items, facilitating type safety and better code understanding in the project.

---
+++ src/assets/ts/DatabaseItem.ts
@@ -0,0 +1,41 @@
+export interface Column
+{
+    name: string;
+    visible: boolean;
+    real_name: string;
+    attributes: string[];
+}
+
+export interface PrintForm
+{
+    year: string;
+    label: string;
+    enabled: boolean;
+    "show-retail": boolean;
+    "price-column": string;
+    "show-price-label": boolean;
+    "retail-price-column": string;
+}
+
+export interface Options
+{
+    columns: Column[];
+    "print-form": PrintForm;
+    "voice-search": boolean;
+    "mardens-price": any[];
+    "add-if-missing": boolean;
+    "remove-if-zero": boolean;
+    "allow-additions": boolean;
+    "allow-inventorying": boolean;
+}
+
+export default interface DatabaseItem
+{
+    id: string;
+    name: string;
+    location: string;
+    po: string;
+    image: string;
+    options: Options;
+    post_date: string;
+}
