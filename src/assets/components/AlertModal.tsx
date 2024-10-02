aee820c3582cccc8732e8a88bfbad6f4af6df970
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-02 16:13:18 -0400

    Add AlertModal component
    Introduce a new AlertModal component to display modal alerts. This component includes customizable message, open/close functionality, and optional action buttons. It uses NextUI for modal and button elements.

---
+++ src/assets/components/AlertModal.tsx
@@ -0,0 +1,32 @@
+import {ReactElement} from "react";
+import {Button, ButtonProps, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader} from "@nextui-org/react";
+
+interface AlertModalProps
+{
+    isOpen: boolean;
+    onClose: () => void;
+    message: string;
+    buttons?: ReactElement<ButtonProps>[] | ReactElement<ButtonProps>;
+}
+
+export default function AlertModal(props: AlertModalProps)
+{
+    return (
+        <Modal
+            isOpen={props.isOpen}
+            onClose={props.onClose}
+            title={"Alert"}
+            size={"sm"}
+        >
+            <ModalContent>
+                <ModalHeader>Alert</ModalHeader>
+                <ModalBody>
+                    <p>{props.message}</p>
+                </ModalBody>
+                <ModalFooter>
+                    {props.buttons ?? <Button onClick={props.onClose}>Close</Button>}
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    );
+}
