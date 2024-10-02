01bf59457302db49d1f9f6e857dc2ddfd5d90756
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-02 16:12:54 -0400

    Add logout functionality and improve Navigation component
    Introduced a logout alert modal and logout button accessible when logged in. Enhanced Navigation component by adding conditional rendering based on user authentication status, and improved tooltip and link functionalities.

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -1,15 +1,19 @@
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
 import Logo from "../images/Logo.svg.tsx";
-import {Button, Input, Tooltip} from "@nextui-org/react";
+import {Button, Input, Link, Tooltip} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faLock, faMicrophone, faMoon, faSun} from "@fortawesome/free-solid-svg-icons";
+import {faLock, faMicrophone, faMoon, faPlus, faSignOut, faSun} from "@fortawesome/free-solid-svg-icons";
 import {applyTheme, getCurrentTheme, Theme} from "../ts/Theme.ts";
 import {useEffect, useState} from "react";
 import LoginModal from "./LoginModal/LoginModal.tsx";
+import AlertModal from "./AlertModal.tsx";
+import {useAuth} from "./AuthProvider.tsx";
 
 export default function Navigation()
 {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const {auth, isLoggedIn, setIsLoggedIn} = useAuth();
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+    const [isLogoutAlertOpen, setIsLogoutAlertOpen] = useState(false);
     const [darkMode, setDarkMode] = useState(getCurrentTheme() === Theme.dark);
     useEffect(() =>
     {
@@ -19,19 +23,40 @@ export default function Navigation()
 
     return (
         <>
-            <LoginModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}/>
+            <LoginModal isOpen={isLoginModalOpen} onClose={() => setIsLoginModalOpen(false)}/>
+            <AlertModal
+                isOpen={isLogoutAlertOpen}
+                onClose={() =>
+                {
+                    setIsLogoutAlertOpen(false);
+                }}
+                message={"Are you sure you want to logout"}
+                buttons={
+                    <>
+                        <Button radius={"full"} color={"danger"} onClick={() =>
+                        {
+                            auth.logout();
+                            setIsLoggedIn(false);
+                            setIsLogoutAlertOpen(false);
+                        }}>Logout</Button>
+                        <Button radius={"full"} onClick={() => setIsLogoutAlertOpen(false)}>Cancel</Button>
+                    </>
+                }
+            />
             <Navbar maxWidth={"full"} height={80}>
                 <NavbarContent>
-                    <NavbarBrand className={"flex flex-row gap-2"}>
-                        <Logo size={72}/>
-                        <div className={"flex flex-col"}>
-                            <p className="font-bold text-inherit">Pricing Database</p>
-                            <p className="opacity-50 italic">v0.2.3</p>
-                        </div>
-                    </NavbarBrand>
+                    <Tooltip content={"Navigate back home"} delay={1500}>
+                        <NavbarBrand className={"flex flex-row gap-2 dark:text-white text-black"} as={Link} href={"/"}>
+                            <Logo size={72}/>
+                            <div className={"flex flex-col"}>
+                                <p className="font-bold">Pricing Database</p>
+                                <p className="opacity-50 italic">v0.2.3</p>
+                            </div>
+                        </NavbarBrand>
+                    </Tooltip>
                 </NavbarContent>
 
-                <NavbarContent className="hidden sm:flex gap-4 w-full" justify="center">
+                <NavbarContent className="hidden sm:flex gap-2 w-full" justify="center">
                     <Input
                         label={"Search"}
                         placeholder="Search for a database"
@@ -47,14 +72,35 @@ export default function Navigation()
                         </Button>
                     </Tooltip>
                 </NavbarContent>
-                <NavbarContent justify="end">
-                    <NavbarItem>
-                        <Tooltip content={"Login"}>
-                            <Button radius={"full"} className={"h-12 w-12 aspect-square p-0 min-w-0"} onClick={() => setIsModalOpen(true)}>
-                                <FontAwesomeIcon icon={faLock}/>
-                            </Button>
-                        </Tooltip>
-                    </NavbarItem>
+                <NavbarContent justify="end" className={"gap-2"}>
+                    {isLoggedIn ? (
+                        <div className={"flex flex-row gap-2"}>
+                            {auth.getUserProfile().admin && (
+                                <NavbarItem>
+                                    <Tooltip content={"Add new database"}>
+                                        <Button radius={"full"} className={"h-12 w-12 aspect-square p-0 min-w-0"} as={Link} href={"/new"}>
+                                            <FontAwesomeIcon icon={faPlus}/>
+                                        </Button>
+                                    </Tooltip>
+                                </NavbarItem>
+                            )}
+                            <NavbarItem>
+                                <Tooltip content={"Logout"}>
+                                    <Button radius={"full"} className={"h-12 w-12 aspect-square p-0 min-w-0"} onClick={() => setIsLogoutAlertOpen(true)}>
+                                        <FontAwesomeIcon icon={faSignOut}/>
+                                    </Button>
+                                </Tooltip>
+                            </NavbarItem>
+                        </div>
+                    ) : (
+                        <NavbarItem>
+                            <Tooltip content={"Login"}>
+                                <Button radius={"full"} className={"h-12 w-12 aspect-square p-0 min-w-0"} onClick={() => setIsLoginModalOpen(true)}>
+                                    <FontAwesomeIcon icon={faLock}/>
+                                </Button>
+                            </Tooltip>
+                        </NavbarItem>
+                    )}
                     <NavbarItem>
                         <Tooltip content={`Toggle ${darkMode ? "Light" : "Dark"} Mode`}>
                             <Button radius={"full"} className={"h-12 w-12 aspect-square p-0 min-w-0"} onClick={() => setDarkMode(prev => !prev)} color={"primary"}>
