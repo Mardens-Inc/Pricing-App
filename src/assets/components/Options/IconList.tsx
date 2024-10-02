70b382707970f6e266e94d788bf3c5e4c4bf1c5a
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-02 16:13:57 -0400

    Add IconList component
    Introduces the IconList component with a loading state and icon selection functionality. This component fetches icon data from an API and allows users to select and highlight icons.

---
+++ src/assets/components/Options/IconList.tsx
@@ -0,0 +1,80 @@
+import {useEffect, useState} from "react";
+import {baseUrl} from "../../../main.tsx";
+import $ from "jquery";
+import {cn, Image, Skeleton} from "@nextui-org/react";
+
+interface Icon
+{
+    name: string;
+    url: string;
+    file: string;
+}
+
+interface IconListProps
+{
+    onSelect?: (icon: Icon | null) => void;
+    value?: Icon;
+}
+
+export default function IconList(props: IconListProps)
+{
+    const [selected, setSelected] = useState<Icon | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [icons, setIcons] = useState<Icon[]>([]);
+    useEffect(() =>
+    {
+        setIsLoading(true);
+        const url = `${baseUrl}/api/locations/images`;
+        $.get(url).then((data) =>
+        {
+            setIcons(data as Icon[]);
+            setIsLoading(false);
+        });
+    }, []);
+    return (
+        <div className={"w-full"}>
+            <div className={"flex flex-row flex-wrap justify-evenly gap-2 mx-4 grow dark:bg-neutral-700 bg-neutral-200 rounded-md max-h-[400px] overflow-y-auto p-2"}>
+                {isLoading ?
+                    (
+                        Array.from({length: 40}).map((_, i) => (
+                            <div key={i} className={"flex flex-col gap-1 p-4"}>
+                                <Skeleton className={"w-[128px] h-[128px] rounded-md "}/>
+                                <Skeleton className={"h-4 w-full rounded-md"}/>
+                            </div>
+                        ))
+                    ) :
+                    (icons.map((icon) => (
+                        <div
+                            key={icon.name}
+                            data-selected={selected?.name === icon.name}
+                            className={
+                                cn(
+                                    "flex flex-col items-center",
+                                    "dark:hover:bg-white/10 hover:bg-black/10",
+                                    "cursor-pointer p-4 rounded-lg transition-colors",
+                                    "border-[2px] data-[selected=true]:border-primary border-transparent data-[selected=true]:!bg-primary/30"
+                                )
+                            }
+                            onClick={() =>
+                            {
+                                if (selected?.name === icon.name)
+                                {
+                                    if (props.onSelect) props.onSelect(null);
+                                    setSelected(null);
+                                } else
+                                {
+                                    if (props.onSelect) props.onSelect(icon);
+                                    setSelected(icon);
+                                }
+                            }}
+                        >
+                            <Image src={icon.url} alt={icon.name} width={128} height={128} className={"object-center object-contain"}/>
+                            <span>{icon.name}</span>
+                        </div>
+                    )))
+
+                }
+            </div>
+        </div>
+    );
+}
