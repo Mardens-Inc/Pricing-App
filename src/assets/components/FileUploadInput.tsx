f5d752cbfbc2986db3eb0e751869575462bf5b33
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-02 16:13:03 -0400

    Add FileUploadInput component for CSV uploads
    Introduced a new FileUploadInput component that supports drag-and-drop as well as manual file selection for CSV files. It includes basic styling and event handling to log file details and invoke an optional callback on file upload.

---
+++ src/assets/components/FileUploadInput.tsx
@@ -0,0 +1,89 @@
+import {Button, cn, Divider} from "@nextui-org/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUpload} from "@fortawesome/free-solid-svg-icons";
+import $ from "jquery";
+import {useEffect} from "react";
+
+interface FileUploadInputProps
+{
+    onFileUpload?: (file: File) => void;
+}
+
+export default function FileUploadInput(props: FileUploadInputProps)
+{
+    const id = `drag-drop-file-input-${Math.random().toString(36).substring(7)}`;
+    const openSelector = () =>
+    {
+        const input = $("<input type='file' accept='.csv'>");
+        input.on("change", (e) =>
+        {
+            if (e.target instanceof HTMLInputElement && e.target.files)
+            {
+                const file = e.target.files[0];
+                console.log(file);
+            }
+        });
+        input.trigger("click");
+    };
+
+    useEffect(() =>
+    {
+        const dropArea = $(`#${id}`);
+        dropArea.on("dragover", (e) =>
+        {
+            e.preventDefault();
+            dropArea.attr("data-dragged", "");
+
+        });
+        dropArea.on("dragleave", () =>
+        {
+            dropArea.removeAttr("data-dragged");
+        });
+
+        dropArea.on("drop", (e) =>
+        {
+            e.preventDefault();
+            dropArea.removeAttr("data-dragged");
+            if (e.originalEvent instanceof DragEvent && e.originalEvent.dataTransfer)
+            {
+                const file = e.originalEvent.dataTransfer.files[0];
+                console.log(file);
+                if (props.onFileUpload) props.onFileUpload(file);
+            }
+        });
+
+    }, []);
+
+    return (
+        <div
+            id={id}
+            className={
+                cn(
+                    "dark:outline-2 outline-3 outline-dotted dark:outline-white/50 outline-black/50",
+                    "h-[200px] rounded-2xl flex flex-row p-4 gap-8 items-center justify-center",
+                    "shadow-inner m-4 transition-colors",
+                    "data-[dragged]:!bg-primary/10"
+                )
+            }
+        >
+            <div className={"flex flex-col"}>
+                <p className={"text-4xl font-bold text-center px-4"}>Drag &amp; Drop</p>
+                <p className={"text-center italic"}>(.csv)</p>
+            </div>
+            <Divider orientation={"vertical"} className={"mx-[100px]"}/>
+            <div className={"flex flex-col"}>
+                <Button
+                    size={"lg"}
+                    variant={"ghost"}
+                    radius={"full"}
+                    color={"primary"}
+                    className={"p-8"}
+                    onClick={openSelector}
+                    startContent={<FontAwesomeIcon icon={faUpload}/>}
+                >
+                    Select CSV
+                </Button>
+            </div>
+        </div>
+    );
+}
