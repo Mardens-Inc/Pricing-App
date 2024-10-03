10fee122810fd05ecad0d85f518cf17e125440aa
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-03 16:28:24 -0400

    Add extra options and enhance DatabaseOptionsPage
    Introduced new options such as AllowInventorying, CanPrintLabel, and ExtendedSwitch for enabling voice search. Updated the Cancel button to navigate conditionally based on 'isNew' status. Additional refactor includes importing necessary components and corrected the import path for useAuth.

--- src/assets/pages/DatabaseOptionsPage.tsx
+++ src/assets/pages/DatabaseOptionsPage.tsx
@@ -1,10 +1,13 @@
 import {useNavigate, useParams} from "react-router-dom";
-import {Button, Input} from "@nextui-org/react";
+import {Button, Input, Link} from "@nextui-org/react";
 import IconList from "../components/Options/IconList.tsx";
 import FileUploadInput from "../components/Options/FileUploadInput.tsx";
-import {useAuth} from "../components/AuthProvider.tsx";
+import {useAuth} from "../providers/AuthProvider.tsx";
 import {isProduction} from "../../main.tsx";
 import ColumnsList from "../components/Options/ColumnsList.tsx";
+import AllowInventorying from "../components/Options/AllowInventorying.tsx";
+import CanPrintLabel from "../components/Options/CanPrintLabel.tsx";
+import ExtendedSwitch from "../components/Extends/ExtendedSwitch.tsx";
 
 export default function DatabaseOptionsPage()
 {
@@ -55,11 +58,19 @@ export default function DatabaseOptionsPage()
                 </div>
 
                 <ColumnsList/>
+                <h3 className={"text-2xl"}>Extra Options</h3>
+                <AllowInventorying/>
+                <CanPrintLabel/>
+                <ExtendedSwitch
+                    label={"Enable voice search?"}
+                    description={"Allow users to search using voice commands."}
+                    className={"max-w-full"}
+                />
 
             </div>
             <div className={"flex flex-row items-center justify-end gap-2 fixed bottom-0 h-20 w-full pr-4 z-10 backdrop-blur-lg backdrop-saturate-150 bg-background/70"}>
                 <Button radius={"full"} color={"primary"}>Save</Button>
-                <Button radius={"full"}>Cancel</Button>
+                <Button radius={"full"} as={Link} href={isNew ? "/" : `/${id}`}>Cancel</Button>
             </div>
         </div>
     );
