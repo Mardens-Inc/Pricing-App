ce6afc0629a33561e110876e538bbddfd36892c8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-28 16:28:40 -0400

    Standardize formatting in tailwind.config.js
    Added consistent spacing and alignment throughout the file to enhance readability and maintain consistency. No functional changes were made to the configuration settings.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,39 +2,41 @@ import {nextui} from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
+    content:  [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
         "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
     ],
-    theme: {
+    theme:    {
         extend: {},
     },
     darkMode: "class",
-    plugins: [nextui({
-        themes: {
-            light: {
-                colors: {
-                    primary: {
-                        DEFAULT: "#ec2b37",
-                        foreground: "#fff",
-                    },
-                    secondary: "#2b2b2b",
-                    background: "#ffffff",
+    plugins:  [
+        nextui({
+            themes: {
+                light: {
+                    colors: {
+                        primary:    {
+                            DEFAULT:    "#ec2b37",
+                            foreground: "#fff",
+                        },
+                        secondary:  "#2b2b2b",
+                        background: "#ffffff",
 
-                }
-            },
-            dark: {
-                colors: {
-                    primary: {
-                        DEFAULT: "#ec2b37",
-                        foreground: "#fff",
-                    },
-                    // primary: "#f13848",
-                    secondary: "#eaeaea",
-                    background: "#0b0b0b",
-                }
-            },
-        }
-    })]
+                    }
+                },
+                dark:  {
+                    colors: {
+                        primary: {
+                            DEFAULT:    "#ec2b37",
+                            foreground: "#fff",
+                        },
+                        // primary: "#f13848",
+                        secondary:  "#eaeaea",
+                        background: "#0b0b0b",
+                    }
+                },
+            }
+        })
+    ]
 }
