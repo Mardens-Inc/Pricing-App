ef6a333d9abefebcedbb667545ae2b644d4ac498
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-30 08:17:10 -0400

    Add single and multiple selection modes to column attributes
    Introduced the `selectionMode` property to `ColumnAttribute` interface and updated default attributes. Added functionality to enforce single selection mode across columns, ensuring only one attribute with `selectionMode: "single"` can be selected at a time.

--- src/assets/components/Options/ColumnAttributes.tsx
+++ src/assets/components/Options/ColumnAttributes.tsx
@@ -1,8 +1,9 @@
-import {Button, Tooltip} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
 import {fa1, faAlignJustify, faKey, faLayerGroup, faLock, faMagnifyingGlass, faPercent, faShop, faTag} from "@fortawesome/free-solid-svg-icons";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import {IndexedColumnItem} from "./ColumnsList.tsx";
+import {Button, Tooltip} from "@nextui-org/react";
 
 export interface ColumnAttribute
 {
@@ -10,6 +11,7 @@ export interface ColumnAttribute
     description: string | React.ReactNode;
     icon: IconProp;
     selected: boolean;
+    selectionMode: "single" | "multiple";
 }
 
 interface ColumnAttributesProps
@@ -18,78 +20,91 @@ interface ColumnAttributesProps
     onSelectionChange?: (attribute: string[]) => void;
 }
 
+export const DefaultAttributes: ColumnAttribute[] = [
+    {
+        key: "primary",
+        description: (
+            <p>Mark this item as the primary key, which will be used to uniquely identify each item.<br/><b>There can only be one primary key.</b></p>
+        ),
+        icon: faKey,
+        selected: false,
+        selectionMode: "single"
+    },
+    {
+        key: "price",
+        description: "Mark this item as a price column, which will be used to format it as currency.",
+        icon: faTag,
+        selected: false,
+        selectionMode: "multiple"
+    },
+    {
+        key: "search",
+        description: "Mark this item as a search column, which will be used to search for items.",
+        icon: faMagnifyingGlass,
+        selected: false,
+        selectionMode: "multiple"
+    },
+    {
+        key: "quantity",
+        description: (
+            <p>Mark this item as a quantity column, which will be used to calculate the total price of an item and incrementing and decrementing inventory.<br/><b>There can only be one quantity column.</b></p>
+        ),
+        icon: fa1,
+        selected: false,
+        selectionMode: "single"
+    },
+    {
+        key: "description",
+        description: (
+            <p>Mark this as the description column, this will be used for voice search.<br/><b>There can only be one description column.</b></p>
+        ),
+        icon: faAlignJustify,
+        selected: false,
+        selectionMode: "single"
+    },
+    {
+        key: "department",
+        description: (
+            <p>Mark this as the department column, which will be used in printing.<br/><b>There can only be one department column.</b></p>
+        ),
+        icon: faShop,
+        selected: false,
+        selectionMode: "single"
+    },
+    {
+        key: "mardensPrice",
+        description: (
+            <p>Mark this as the Marden's Price column.<br/><b>There can only be one Marden's Price column.</b></p>
+        ),
+        icon: faPercent,
+        selected: false,
+        selectionMode: "single"
+    },
+    {
+        key: "category",
+        description: (
+            <p>Mark this column for use in the <b>Dynamically Generate Mardens Price</b> section.<br/><b>There can only be one category column.</b></p>
+        ),
+        icon: faLayerGroup,
+        selected: false,
+        selectionMode: "single"
+    },
+    {
+        key: "readonly",
+        description: "Mark this item as readonly, which will prevent it from being edited and remove it from the addition form.",
+        icon: faLock,
+        selected: false,
+        selectionMode: "multiple"
+    }
+];
 export default function ColumnAttributes(props: ColumnAttributesProps)
 {
-    const attributes: ColumnAttribute[] = [
-        {
-            key: "primaryKey",
-            description: (
-                <p>Mark this item as the primary key, which will be used to uniquely identify each item.<br/><b>There can only be one primary key.</b></p>
-            ),
-            icon: faKey,
-            selected: props.selected?.some(attr => attr === "primaryKey") ?? false
-        },
-        {
-            key: "price",
-            description: "Mark this item as a price column, which will be used to format it as currency.",
-            icon: faTag,
-            selected: props.selected?.some(attr => attr === "price") ?? false
-        },
-        {
-            key: "search",
-            description: "Mark this item as a search column, which will be used to search for items.",
-            icon: faMagnifyingGlass,
-            selected: props.selected?.some(attr => attr === "search") ?? false
-        },
-        {
-            key: "quantity",
-            description: (
-                <p>Mark this item as a quantity column, which will be used to calculate the total price of an item and incrementing and decrementing inventory.<br/><b>There can only be one quantity column.</b></p>
-            ),
-            icon: fa1,
-            selected: props.selected?.some(attr => attr === "quantity") ?? false
-        },
-        {
-            key: "description",
-            description: (
-                <p>Mark this as the description column, this will be used for voice search.<br/><b>There can only be one description column.</b></p>
-            ),
-            icon: faAlignJustify,
-            selected: props.selected?.some(attr => attr === "description") ?? false
-        },
-        {
-            key: "department",
-            description: (
-                <p>Mark this as the department column, which will be used in printing.<br/><b>There can only be one department column.</b></p>
-            ),
-            icon: faShop,
-            selected: props.selected?.some(attr => attr === "department") ?? false
-        },
-        {
-            key: "mardensPrice",
-            description: (
-                <p>Mark this as the Marden's Price column.<br/><b>There can only be one Marden's Price column.</b></p>
-            ),
-            icon: faPercent,
-            selected: props.selected?.some(attr => attr === "mardensPrice") ?? false
-        },
-        {
-            key: "category",
-            description: (
-                <p>Mark this column for use in the <b>Dynamically Generate Mardens Price</b> section.<br/><b>There can only be one category column.</b></p>
-            ),
-            icon: faLayerGroup,
-            selected: props.selected?.some(attr => attr === "category") ?? false
-        },
-        {
-            key: "readonly",
-            description: "Mark this item as readonly, which will prevent it from being edited and remove it from the addition form.",
-            icon: faLock,
-            selected: props.selected?.some(attr => attr === "readonly") ?? false
-        }
-    ];
+    const attributes: ColumnAttribute[] = [...DefaultAttributes];
+    for (const attribute of attributes)
+    {
+        attribute.selected = props.selected?.some(attr => attr === attribute.key) ?? attribute.selected;
+    }
 
-    console.log("Attributes: ", attributes);
 
     return (
         <div className="attributes flex flex-row gap-1">
@@ -118,4 +133,31 @@ export default function ColumnAttributes(props: ColumnAttributesProps)
             ))}
         </div>
     );
+}
+
+export function enforceAttributesSingleSelectionMode(columns: IndexedColumnItem[]): IndexedColumnItem[]
+{
+    for (const column of columns)
+    {
+        if (!column?.attributes || column.attributes.length === 0) continue;
+        const attributes = column.attributes.map(i => DefaultAttributes.find(j => j.key === i)).filter(i => i);
+        if (!attributes) continue;
+
+        for (const attribute of attributes)
+        {
+            if (!attribute) continue;
+            if (attribute.selectionMode === "single")
+            {
+                const otherColumns = columns.filter(i => i.id !== column.id && i.attributes.includes(attribute.key));
+                for (const otherColumn of otherColumns)
+                {
+                    otherColumn.attributes = otherColumn.attributes.filter(i => i !== attribute.key);
+                    console.log("Enforcing single selection mode: ", otherColumn);
+                }
+
+            }
+        }
+    }
+
+    return columns;
 }
