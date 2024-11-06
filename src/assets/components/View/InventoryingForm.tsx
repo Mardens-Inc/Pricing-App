44a2e2d62193043886fdbe216d2db52b2e1f629f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-06 16:29:14 -0500

    Add InventoryingForm component for inventory form input
    This component includes input fields for dynamic columns and a submit button that triggers an onSubmit callback. It filters out columns marked as readonly and resets the form after submission.

---
+++ src/assets/components/View/InventoryingForm.tsx
@@ -0,0 +1,42 @@
+import {Button, Input, ScrollShadow} from "@nextui-org/react";
+import {Column} from "../../ts/DatabaseRecords.ts";
+import {useState} from "react";
+
+
+interface InventoryingFormProps
+{
+    columns: Column[];
+    onSubmit: (data: ColumnValue) => void;
+}
+
+export type ColumnValue = {
+    [key: string]: string;
+}
+
+export default function InventoryingForm(props: InventoryingFormProps)
+{
+    const [columnValue, setColumnValue] = useState<ColumnValue>({});
+    return (
+        <ScrollShadow className={"min-w-[200px] w-1/3 max-w-[500px] flex flex-col gap-4 mr-6 max-h-[calc(100dvh_-_250px)] min-h-[250px] overflow-y-auto pb-4"}>
+            <h1>Inventorying</h1>
+            {props.columns.filter(i => !i.attributes.includes("readonly")).map((column) => (
+                <>
+                    <Input
+                        key={column.name}
+                        label={column.name}
+                        placeholder={column.name}
+                        type={"text"}
+                        radius={"full"}
+                        onValueChange={(value) => setColumnValue({...columnValue, [column.real_name]: value})}
+                        value={columnValue[column.real_name] || ""}
+                    />
+                </>
+            ))}
+            <Button radius={"full"} color={"primary"} className={"w-1/2 mx-auto h-10 min-h-10"} onClick={() =>
+            {
+                props.onSubmit(columnValue);
+                setColumnValue({});
+            }}>Update</Button>
+        </ScrollShadow>
+    );
+}
