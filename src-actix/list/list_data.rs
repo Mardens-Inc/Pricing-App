9f936dc6d84a3b361f2cea79885d1a47b782e53b
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-07 16:27:53 -0500

    Refactor LocationListItem struct and add new structs
    Refactor the LocationListItem struct with a new post_date field and add LocationListItemNoId and HashedLocationListItem structs. These changes improve flexibility and data handling for different use cases. Additionally, add FromRequest implementations to allow parsing from request queries.

--- src-actix/list/list_data.rs
+++ src-actix/list/list_data.rs
@@ -1,24 +1,59 @@
 use actix_web::FromRequest;
 use serde::{Deserialize, Serialize};
-use sqlx::types::chrono::NaiveDateTime;
 
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
 pub struct LocationListItem {
+	pub id: u32,
+	pub name: String,
+	pub location: String,
+	pub po: String,
+	pub image: String,
+	pub post_date: sqlx::types::chrono::NaiveDateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LocationListItemNoId {
+	pub name: String,
+	pub location: String,
+	pub po: String,
+	pub image: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HashedLocationListItem {
 	pub id: String,
 	pub name: String,
 	pub location: String,
 	pub po: String,
 	pub image: String,
-	pub created_date: NaiveDateTime,
+	pub post_date: sqlx::types::chrono::NaiveDateTime,
 }
 
-impl FromRequest for LocationListItem{
+impl FromRequest for LocationListItem {
 	type Error = actix_web::Error;
 	type Future = futures::future::Ready<Result<Self, Self::Error>>;
-	fn from_request(req: &actix_web::HttpRequest, _payload: &mut actix_web::dev::Payload) -> Self::Future {
+	fn from_request(
+		req: &actix_web::HttpRequest,
+		_payload: &mut actix_web::dev::Payload,
+	) -> Self::Future {
 		futures::future::ready(
 			serde_urlencoded::from_str::<Self>(req.query_string())
-				.map_err(|err| actix_web::error::ErrorBadRequest(err.to_string()))
+				.map_err(|err| actix_web::error::ErrorBadRequest(err.to_string())),
 		)
 	}
 }
+
+impl FromRequest for LocationListItemNoId {
+	type Error = actix_web::Error;
+	type Future = futures::future::Ready<Result<Self, Self::Error>>;
+	fn from_request(
+		req: &actix_web::HttpRequest,
+		_payload: &mut actix_web::dev::Payload,
+	) -> Self::Future {
+		futures::future::ready(
+			serde_urlencoded::from_str::<Self>(req.query_string())
+				.map_err(|err| actix_web::error::ErrorBadRequest(err.to_string())),
+		)
+	}
+}
+
