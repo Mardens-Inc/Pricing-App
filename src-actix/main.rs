228f4fc25b309f2873caad3eb6ef5262ffa1de03
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-07 11:26:39 -0500

    Refactor server initialization and add database error handling
    Reorganized the Actix server initialization to support different environments. Introduced additional modules and incorporated error handling for database connection failures. Simplified the log level and revised the development check mechanism.

--- src-actix/main.rs
+++ src-actix/main.rs
@@ -3,37 +3,51 @@ mod all_databases_endpoint;
 mod all_databases;
 mod singular_database;
 
+#[path = "list/list_db.rs"]
+mod list_db;
+#[path = "list/list_data.rs"]
+mod list_data;
+#[path = "list/list_endpoint.rs"]
+mod list_endpoint;
+mod data_database_connection;
+
+use crate::data_database_connection::DatabaseConnectionData;
 use actix_web::{get, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use awc::Client;
 use futures_util::stream::StreamExt;
 use include_dir::{include_dir, Dir};
-use log::info;
+use log::{error, info};
 use serde_json::json;
 
-
-pub static DEV: bool = cfg!(debug_assertions);
+pub static DEBUG: bool = cfg!(debug_assertions);
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	std::env::set_var("RUST_LOG", "trace");
+	std::env::set_var("RUST_LOG", "info");
 	env_logger::init();
 
 	let port = 1860; // Port to listen on
-	let config = if DEV {
-		"development"
+
+
+	let data = if DEBUG {
+		None
 	} else {
-		"production"
+		Some(match DatabaseConnectionData::get().await {
+			Ok(d) => d,
+			Err(err) => {
+				error!("Failed to get database connection data: {}", err);
+				return Ok(());
+			}
+		})
 	};
 
-//	match all_databases::initialize_database().await {
-//		Ok(_) => info!("Locations database initialized"),
-//		Err(e) => info!("Error initializing the locations database: {}", e)
-//	}
+	let data = data.as_ref();
+	list_db::initialize(data).await.map_err(|err| {
+		error!("Failed to initialize database: {}", err);
+		std::io::Error::new(std::io::ErrorKind::Other, "Failed to initialize database")
+	})?;
 
-	info!("Starting {} server at http://127.0.0.1:{}", config, port);
-
-
-	HttpServer::new(move || {
+	let server = HttpServer::new(move || {
 		let app = App::new()
 			.wrap(middleware::Logger::default())
 			.app_data(
@@ -50,7 +64,7 @@ async fn main() -> std::io::Result<()> {
 			.service(web::scope("api").service(status));
 
 		// Add conditional routing based on the config
-		if config == "development" {
+		if DEBUG {
 			app.default_service(web::route().to(proxy_to_vite))
 			   .service(
 				   web::resource("/assets/{file:.*}")
@@ -67,37 +81,39 @@ async fn main() -> std::io::Result<()> {
 	})
 		.workers(4)
 		.bind(format!("0.0.0.0:{port}", port = port))?
-		.run()
-		.await
+		.run();
+
+	info!("Starting {} server at http://127.0.0.1:{}", if DEBUG {"development"} else {"production"}, port);
+	server.await
 }
 
 
-/// The maximum payload size allowed for forwarding requests and responses.
-///
-/// This constant defines the maximum size (in bytes) for the request and response payloads
-/// when proxying. Any payload exceeding this size will result in an error.
-///
-/// Currently, it is set to 1 GB.
+// The maximum payload size allowed for forwarding requests and responses.
+//
+// This constant defines the maximum size (in bytes) for the request and response payloads
+// when proxying. Any payload exceeding this size will result in an error.
+//
+// Currently, it is set to 1 GB.
 const MAX_PAYLOAD_SIZE: usize = 1024 * 1024 * 1024; // 1 GB
 
-/// Static directory including all files under `target/wwwroot`.
-///
-/// This static directory is used to embed files into the binary at compile time.
-/// The `WWWROOT` directory will be used to serve static files such as `index.html`.
+// Static directory including all files under `target/wwwroot`.
+//
+// This static directory is used to embed files into the binary at compile time.
+// The `WWWROOT` directory will be used to serve static files such as `index.html`.
 static WWWROOT: Dir = include_dir!("target/wwwroot");
-/// Handles the request for the index.html file.
-///
-/// This function serves the `index.html` file from the embedded directory
-/// if it exists, and returns an internal server error if the file is not found.
-///
-/// # Arguments
-///
-/// * `_req` - The HTTP request object.
-///
-/// # Returns
-///
-/// An `impl Responder` which can either be a successful HTTP response containing
-/// the `index.html` file, or an internal server error.
+// Handles the request for the index.html file.
+//
+// This function serves the `index.html` file from the embedded directory
+// if it exists, and returns an internal server error if the file is not found.
+//
+// # Arguments
+//
+// * `_req` - The HTTP request object.
+//
+// # Returns
+//
+// An `impl Responder` which can either be a successful HTTP response containing
+// the `index.html` file, or an internal server error.
 async fn index(_req: HttpRequest) -> Result<impl Responder, Error> {
 	if let Some(file) = WWWROOT.get_file("index.html") {
 		let body = file.contents();
@@ -106,21 +122,21 @@ async fn index(_req: HttpRequest) -> Result<impl Responder, Error> {
 	Err(actix_web::error::ErrorInternalServerError("Failed to find index.html"))
 }
 
-/// Proxies requests to the Vite development server.
-///
-/// This function forwards incoming requests to a local Vite server running on port 3000.
-/// It buffers the entire request payload and response payload to avoid partial transfers.
-/// Requests and responses larger than the maximum payload size will result in an error.
-///
-/// # Arguments
-///
-/// * `req` - The HTTP request object.
-/// * `payload` - The request payload.
-///
-/// # Returns
-///
-/// An `HttpResponse` which contains the response from the Vite server,
-/// or an error response in case of failure.
+// Proxies requests to the Vite development server.
+//
+// This function forwards incoming requests to a local Vite server running on port 3000.
+// It buffers the entire request payload and response payload to avoid partial transfers.
+// Requests and responses larger than the maximum payload size will result in an error.
+//
+// # Arguments
+//
+// * `req` - The HTTP request object.
+// * `payload` - The request payload.
+//
+// # Returns
+//
+// An `HttpResponse` which contains the response from the Vite server,
+// or an error response in case of failure.
 async fn proxy_to_vite(req: HttpRequest, mut payload: web::Payload) -> Result<HttpResponse, Error> {
 	let client = Client::new();
 	let forward_url = format!("http://localhost:3000{}", req.uri());
@@ -172,15 +188,15 @@ async fn proxy_to_vite(req: HttpRequest, mut payload: web::Payload) -> Result<Ht
 	Ok(res.body(resp_body_bytes))
 }
 
-/// Handles requests to check the server status.
-///
-/// This endpoint responds to GET requests with a JSON object indicating
-/// that the server is running correctly. It can be used for health checks
-/// or monitoring server status.
-///
-/// # Returns
-///
-/// A JSON object with a `status` field set to "ok".
+// Handles requests to check the server status.
+//
+// This endpoint responds to GET requests with a JSON object indicating
+// that the server is running correctly. It can be used for health checks
+// or monitoring server status.
+//
+// # Returns
+//
+// A JSON object with a `status` field set to "ok".
 #[get("/")]
 async fn status() -> impl Responder {
 	HttpResponse::Ok().json(json!({ "status": "ok" }))
