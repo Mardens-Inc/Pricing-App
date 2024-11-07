b63cc522aaa18de5d7ad950258246faa4640aa33
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-07 16:27:25 -0500

    Add logging info when fetching database connection data
    Introduced logging to track when the database connection data is being fetched. This helps with debugging and monitoring the configuration retrieval process. Adjusted formatting for consistency.

--- src-actix/data_database_connection.rs
+++ src-actix/data_database_connection.rs
@@ -1,16 +1,16 @@
+use log::info;
 use serde::Deserialize;
 
 #[derive(Deserialize, Clone)]
-pub struct DatabaseConnectionData
-{
+pub struct DatabaseConnectionData {
 	pub host: String,
 	pub user: String,
 	pub password: String,
-	pub hash: String,
 }
 
 impl DatabaseConnectionData {
 	pub(crate) async fn get() -> Result<Self, Box<dyn std::error::Error>> {
+		info!("Fetching database connection data");
 		let url = "https://lib.mardens.com/config.json";
 		let response = reqwest::get(url).await?.json::<Self>().await?;
 		Ok(response)
