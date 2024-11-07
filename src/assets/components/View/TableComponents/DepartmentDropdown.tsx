426983205f92e01a852c55df92e4282614808e9d
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-07 09:51:11 -0500

    Add DepartmentDropdown component
    Introduce a new DepartmentDropdown component to facilitate department selection. This component uses a Select element from @nextui-org/react and integrates with the existing Departments array to update department data attributes dynamically.

---
+++ src/assets/components/View/TableComponents/DepartmentDropdown.tsx
@@ -0,0 +1,40 @@
+import {Departments} from "../../../ts/printer.ts";
+import $ from "jquery";
+import {Select, SelectItem} from "@nextui-org/react";
+
+export default function DepartmentDropdown({id}: { id: string })
+{
+    return (
+        <Select
+            key={`${id}-dept-selector`}
+            label={"Department"}
+            placeholder={"Select a department to print"}
+            radius={"full"}
+            classNames={{}}
+            onSelectionChange={selection =>
+            {
+                const dept = Departments.find(i => i.id === +(
+                    ([...selection][0] as string)
+                        .replace(`${id}-`, "")
+                        .trim()
+                ))?.id ?? -1;
+                $(`tr#${id}`).attr("data-department", dept);
+            }}
+        >
+            {
+                Departments
+                    .filter(i => i.id > 0)
+                    .map(
+                        dept =>
+                            <SelectItem
+                                key={`${id}-${dept.id}`}
+                                value={dept.name}
+                                textValue={dept.name}
+                            >
+                                {dept.id} - {dept.name}
+                            </SelectItem>
+                    )
+            }
+        </Select>
+    );
+}
