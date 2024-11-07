19a66139b9b11618d71cf3cf971f889dc32a71c8
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-07 11:26:27 -0500

    Add DatabaseConnectionData struct with async get method
    Introduces a new `DatabaseConnectionData` struct with deserialization support using `serde`. Implements an async `get` method to fetch and parse configuration from a remote JSON source.

---
+++ src-actix/data_database_connection.rs
@@ -0,0 +1,18 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone)]
+pub struct DatabaseConnectionData
+{
+	pub host: String,
+	pub user: String,
+	pub password: String,
+	pub hash: String,
+}
+
+impl DatabaseConnectionData {
+	pub(crate) async fn get() -> Result<Self, Box<dyn std::error::Error>> {
+		let url = "https://lib.mardens.com/config.json";
+		let response = reqwest::get(url).await?.json::<Self>().await?;
+		Ok(response)
+	}
+}
