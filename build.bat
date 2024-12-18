c83273f7795585e3f181d76b40a7138727f8308e
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-12-18 16:12:24 -0500

    Add build script for pricing app deployment
    This script facilitates building and deploying the pricing app by combining frontend and backend build commands. It uses a specified SSH key and server details for remote operations.

---
+++ build.bat
@@ -0,0 +1,1 @@
+pwp -j 192.168.21.209 -u administrator -a C:\Users\drew.chase\.ssh\mss-sshkey_ossh.key -s pricing-db --binary pricing-app --build-command "npm run build-frontend && wsl cargo build --release" --build
