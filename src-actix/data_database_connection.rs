6642a37cca4fce1f9a3975a90173cdfcac8d3391
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-12-18 16:13:53 -0500

    Refactor database connection fetch logic using Reqwest Client
    Replaced `reqwest::get` with a `Reqwest Client` to allow more robust configuration for HTTP requests, including accepting invalid certificates. This improves flexibility and provides safer control over the request behavior.

--- src-actix/data_database_connection.rs
+++ src-actix/data_database_connection.rs
@@ -1,18 +1,21 @@
-use log::info;
 use serde::Deserialize;
 
 #[derive(Deserialize, Clone)]
 pub struct DatabaseConnectionData {
-	pub host: String,
-	pub user: String,
-	pub password: String,
+    pub host: String,
+    pub user: String,
+    pub password: String,
 }
 
 impl DatabaseConnectionData {
-	pub(crate) async fn get() -> Result<Self, Box<dyn std::error::Error>> {
-		info!("Fetching database connection data");
-		let url = "https://lib.mardens.com/config.json";
-		let response = reqwest::get(url).await?.json::<Self>().await?;
-		Ok(response)
-	}
+    pub(crate) async fn get() -> Result<Self, Box<dyn std::error::Error>> {
+        use reqwest::Client;
+        let url = "https://lib.mardens.com/config.json";
+        let client = Client::builder()
+            .danger_accept_invalid_certs(true)
+            .build()?;
+        let response = client.get(url).send().await?;
+        let credentials = response.json::<DatabaseConnectionData>().await?;
+        Ok(credentials)
+    }
 }
