fc013e28baffe7f25f4a82a844493696449f124a
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-12-20 15:49:57 -0500

    Refactor endpoints to use Result for improved error handling
    Replaced custom error responses with the use of `Result` and `Box<dyn Error>` for consistent error propagation. This simplifies the code by removing repetitive error-handling logic and leveraging the `?` operator for cleaner syntax. Updated the `delete` endpoint to decode string IDs using `hashids`.

--- src-actix/list/list_endpoint.rs
+++ src-actix/list/list_endpoint.rs
@@ -1,61 +1,43 @@
 use crate::data_database_connection::DatabaseConnectionData;
 use crate::{list_data, list_db};
 use actix_web::{delete, get, post, web, HttpResponse, Responder};
-use log::error;
+use crypto::hashids::decode_single;
+use std::error::Error;
 use std::sync::Arc;
-use serde_json::json;
 
 #[get("")]
 pub async fn get_all_locations(
-	data: web::Data<Arc<DatabaseConnectionData>>,
-) -> impl Responder {
-	let data = data.get_ref().as_ref();
-	let locations = match list_db::get_all(data).await {
-		Ok(locations) => locations,
-		Err(err) => {
-			error!("Failed to get locations: {}", err);
-			return HttpResponse::InternalServerError().json(json!({
-				"error": "Failed to get locations",
-				"details": format!("{}", err),
-			}));
-		}
-	};
-	HttpResponse::Ok().json(locations)
+    data: web::Data<Arc<DatabaseConnectionData>>,
+) -> Result<impl Responder, Box<dyn Error>> {
+    let data = data.get_ref().as_ref();
+    let locations = list_db::get_all(data).await?;
+    Ok(HttpResponse::Ok().json(locations))
 }
 
 #[post("")]
 pub async fn create_location(
-	data: web::Data<Arc<DatabaseConnectionData>>,
-	body: web::Json<list_data::LocationListItem>
-) -> impl Responder {
-	let data = data.get_ref().as_ref();
-	match list_db::insert(&body, data).await {
-		Ok(_) => HttpResponse::Ok().finish(),
-		Err(err) => {
-			error!("Failed to insert location: {}", err);
-			HttpResponse::InternalServerError().finish()
-		}
-	}
+    data: web::Data<Arc<DatabaseConnectionData>>,
+    body: web::Json<list_data::LocationListItem>,
+) -> Result<impl Responder, Box<dyn Error>> {
+    let data = data.get_ref().as_ref();
+    list_db::insert(&body, data).await?;
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[delete("/{id}")]
 pub async fn delete_location(
-	data: web::Data<Arc<DatabaseConnectionData>>,
-	id: web::Path<u64>,
-) -> impl Responder {
-	let data = data.get_ref().as_ref();
-	let id = *id.as_ref();
+    data: web::Data<Arc<DatabaseConnectionData>>,
+    id: web::Path<String>,
+) -> Result<impl Responder, Box<dyn Error>> {
+    let data = data.get_ref().as_ref();
+    let id = id.as_ref();
+    let id = decode_single(id)?;
 
-	// Delete from the locations list
-	match list_db::delete(id, data).await {
-		Ok(_) => (),
-		Err(err) => {
-			error!("Failed to delete location: {}", err);
-		}
-	};
+    // Delete from the locations list
+    list_db::delete(id, data).await?;
 
-	// Delete the database table
-	// TODO: Implement this
+    // Delete the database table
+    // TODO: Implement this
 
-	HttpResponse::Ok().finish()
+    Ok(HttpResponse::Ok().finish())
 }
