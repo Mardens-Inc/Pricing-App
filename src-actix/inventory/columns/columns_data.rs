ddab36fd4293a2c07c660654beea3e16aefe82fa
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-12-20 15:49:15 -0500

    Add InventoryColumn struct with serialization support
    Introduce `InventoryColumn` struct to represent inventory columns and implement custom serialization for JSON output. Attributes are split into a vector, and `database_id` is encoded for consistent formatting.

---
+++ src-actix/inventory/columns/columns_data.rs
@@ -0,0 +1,34 @@
+use crypto::hashids::encode_single;
+use serde::ser::SerializeMap;
+use serde::{Serialize, Serializer};
+use sqlx::FromRow;
+
+#[derive(FromRow)]
+pub struct InventoryColumn {
+    pub id: i64,
+    pub name: String,
+    pub display_name: Option<String>,
+    pub visible: bool,
+    pub attributes: Option<String>,
+    pub database_id: u64,
+}
+
+impl Serialize for InventoryColumn {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(5))?;
+        map.serialize_entry("name", &self.name)?;
+        map.serialize_entry("display_name", &self.display_name)?;
+        map.serialize_entry("visible", &self.visible)?;
+        map.serialize_entry("database_id", encode_single(self.database_id).as_str())?;
+
+        if let Some(attributes) = &self.attributes {
+            let attributes: Vec<String> = attributes.split(",").map(|s| s.to_string()).collect();
+            map.serialize_entry("attributes", &attributes)?;
+        }
+
+        map.end()
+    }
+}
