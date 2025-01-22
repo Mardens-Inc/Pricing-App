6f784ba4811142fa7353fa7774ca940e014f7048
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-22 09:44:11 -0500

    Add deserialization support for InventoryColumn
    Implemented the `Deserialize` trait for the `InventoryColumn` struct, enabling it to be constructed from serialized data. This includes defining a custom visitor to handle deserialization logic and field validation.

--- src-actix/inventory/columns/columns_data.rs
+++ src-actix/inventory/columns/columns_data.rs
@@ -1,6 +1,8 @@
+use serde::de::{self, Deserializer, MapAccess, Visitor};
 use serde::ser::SerializeMap;
 use serde::{Serialize, Serializer};
 use sqlx::FromRow;
+use std::fmt;
 
 #[derive(FromRow)]
 pub struct InventoryColumn {
@@ -28,5 +30,78 @@ impl Serialize for InventoryColumn {
         }
 
         map.end()
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for InventoryColumn {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct InventoryColumnVisitor;
+
+        impl<'de> Visitor<'de> for InventoryColumnVisitor {
+            type Value = InventoryColumn;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a map representing an InventoryColumn")
+            }
+
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: MapAccess<'de>,
+            {
+                let mut id = None;
+                let mut name = None;
+                let mut display_name = None;
+                let mut visible = None;
+                let mut attributes = None;
+                let mut database_id = None;
+
+                while let Some(key) = map.next_key::<String>()? {
+                    match key.as_str() {
+                        "id" => id = Some(map.next_value()?),
+                        "name" => name = Some(map.next_value()?),
+                        "display_name" => display_name = Some(map.next_value()?),
+                        "visible" => visible = Some(map.next_value()?),
+                        "attributes" => {
+                            let attr: Option<Vec<String>> = map.next_value()?;
+                            attributes = attr.map(|vec| vec.join(","));
+                        }
+                        "database_id" => database_id = Some(map.next_value()?),
+                        _ => {
+                            return Err(de::Error::unknown_field(
+                                &key,
+                                &[
+                                    "id",
+                                    "name",
+                                    "display_name",
+                                    "visible",
+                                    "attributes",
+                                    "database_id",
+                                ],
+                            ))
+                        }
+                    }
+                }
+
+                let id = id.ok_or_else(|| de::Error::missing_field("id"))?;
+                let name = name.ok_or_else(|| de::Error::missing_field("name"))?;
+                let visible = visible.ok_or_else(|| de::Error::missing_field("visible"))?;
+                let database_id =
+                    database_id.ok_or_else(|| de::Error::missing_field("database_id"))?;
+
+                Ok(InventoryColumn {
+                    id,
+                    name,
+                    display_name,
+                    visible,
+                    attributes,
+                    database_id,
+                })
+            }
+        }
+
+        deserializer.deserialize_map(InventoryColumnVisitor)
     }
 }
