4461d8d6ae3c54efb9c1c99e7846570de257f570
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-27 14:07:44 -0500

    Refactor asset handling and integrate vite-actix dependency
    Removed proxy-related logic and streamlined asset handling for clarity. Introduced the `vite-actix` crate to manage Vite server integration and refined debugging configurations. Improved code structure by renaming traits and fixing minor formatting issues.

--- src-actix/lib.rs
+++ src-actix/lib.rs
@@ -3,6 +3,7 @@
 // TODO: Remove for production
 #![allow(clippy::all)]
 // -------------
+use vite_actix::{start_vite_server, AppConfig};
 pub mod data_database_connection;
 mod icons_endpoint;
 mod server_information_endpoint;
@@ -46,7 +47,7 @@ pub mod print_options_data;
 #[path = "inventory/options/print_options/print_options_db.rs"]
 pub mod print_options_db;
 
-use crate::asset_endpoint::{assets, index, proxy_to_vite, start_vite_server, AppConfig};
+use crate::asset_endpoint::AssetsAppConfig;
 use crate::data_database_connection::DatabaseConnectionData;
 use crate::server_information_endpoint::get_server_version;
 use actix_web::{middleware, web, App, HttpResponse, HttpServer};
@@ -59,60 +60,61 @@ const PORT: u16 = 1421;
 
 #[actix_web::main]
 pub async fn run() -> Result<()> {
-	// Set the logging level and initialize the logger
-	std::env::set_var("RUST_LOG", "debug");
-	env_logger::init();
+    // Set the logging level and initialize the logger
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
 
-	// Fetch database connection data
-	let data = DatabaseConnectionData::get().await?;
+    // Fetch database connection data
+    let data = DatabaseConnectionData::get().await?;
 
-	// Initialize necessary databases
-	list_db::initialize(&data).await?;
-	options_db::initialize(&data).await?;
+    // Initialize necessary databases
+    list_db::initialize(&data).await?;
+    options_db::initialize(&data).await?;
 
-	let connection_data_mutex = web::Data::new(std::sync::Arc::new(data));
+    let connection_data_mutex = web::Data::new(std::sync::Arc::new(data));
 
-	let server = HttpServer::new(move || {
-		App::new()
-			.wrap(middleware::Logger::default()) // Add logger middleware
-			.app_data(
-				web::JsonConfig::default()
-					.limit(4096) // Set JSON payload size limit
-					.error_handler(|err, _req| {
-						error!("Failed to parse JSON: {}", err);
-						let error = json!({ "error": format!("{}", err) });
-						actix_web::error::InternalError::from_response(
-							err,
-							HttpResponse::BadRequest().json(error),
-						)
-							.into()
-					}),
-			)
-			.service(
-				web::scope("api")
-					.service(get_server_version)
-					.configure(icons_endpoint::configure)
-					.configure(list_endpoint::configure)
-					.configure(inventory_endpoint::configure)
-					.configure(options_endpoint::configure)
-					.app_data(connection_data_mutex.clone()),
-			)
-			.configure_routes()
-	})
-		.workers(4) // Set number of workers
-		.bind(format!("0.0.0.0:{port}", port = PORT))? // Bind to specified port
-		.run();
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default()) // Add logger middleware
+            .app_data(
+                web::JsonConfig::default()
+                    .limit(4096) // Set JSON payload size limit
+                    .error_handler(|err, _req| {
+                        error!("Failed to parse JSON: {}", err);
+                        let error = json!({ "error": format!("{}", err) });
+                        actix_web::error::InternalError::from_response(
+                            err,
+                            HttpResponse::BadRequest().json(error),
+                        )
+                        .into()
+                    }),
+            )
+            .service(
+                web::scope("api")
+                    .service(get_server_version)
+                    .configure(icons_endpoint::configure)
+                    .configure(list_endpoint::configure)
+                    .configure(inventory_endpoint::configure)
+                    .configure(options_endpoint::configure)
+                    .app_data(connection_data_mutex.clone()),
+            )
+            .configure_routes()
+            .configure_vite()
+    })
+    .workers(4) // Set number of workers
+    .bind(format!("0.0.0.0:{port}", port = PORT))? // Bind to specified port
+    .run();
 
-	info!(
+    info!(
         "Starting {} server at http://127.0.0.1:{}",
         if DEBUG { "development" } else { "production" },
         PORT
     );
 
-	// Start the Vite server in development mode
-	if DEBUG {
-		start_vite_server().expect("Failed to start vite server");
-	}
+    // Start the Vite server in development mode
+    if DEBUG {
+        start_vite_server().expect("Failed to start vite server");
+    }
 
-	Ok(server.await?)
+    Ok(server.await?)
 }
