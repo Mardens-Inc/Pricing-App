9a440ce5a869550e80f8f606ea4c69eaad32ac8d
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-28 14:59:45 -0500

    Remove content from DatabaseOptionsPage component.
    All functionality and UI elements in the DatabaseOptionsPage component have been commented out, leaving an empty return statement. This is likely a preparatory step for refactoring or replacing the content.

--- src/assets/pages/DatabaseOptionsPage.tsx
+++ src/assets/pages/DatabaseOptionsPage.tsx
@@ -1,104 +1,104 @@
-import {useNavigate, useParams} from "react-router-dom";
-import IconList from "../components/Options/IconList.tsx";
-import FileUploadInput from "../components/Options/FileUploadInput.tsx";
-import {useAuth} from "../providers/AuthProvider.tsx";
-import {isProduction, setTitle} from "../../main.tsx";
-import ColumnsList from "../components/Options/ColumnsList.tsx";
-import AllowInventorying from "../components/Options/AllowInventorying.tsx";
-import CanPrintLabel from "../components/Options/CanPrintLabel.tsx";
-import {useEffect, useState} from "react";
-import DatabaseRecords, {Column, DatabaseData} from "../ts/DatabaseRecords.ts";
-import {Button, Input, Link} from "@heroui/react";
+// import {useNavigate, useParams} from "react-router-dom";
+// import IconList from "../components/Options/IconList.tsx";
+// import FileUploadInput from "../components/Options/FileUploadInput.tsx";
+// import {useAuth} from "../providers/AuthProvider.tsx";
+// import {isProduction, setTitle} from "../../main.tsx";
+// import ColumnsList from "../components/Options/ColumnsList.tsx";
+// import AllowInventorying from "../components/Options/AllowInventorying.tsx";
+// import CanPrintLabel from "../components/Options/CanPrintLabel.tsx";
+// import {useEffect, useState} from "react";
+// import {Button, Input, Link} from "@heroui/react";
 
 export default function DatabaseOptionsPage()
 {
-    const navigate = useNavigate();
-    const {isLoggedIn, auth} = useAuth();
-    const id = useParams().id;
-    const [options, setOptions] = useState<DatabaseData | null>(null);
-    const [name, setName] = useState<string>("");
-    const [location, setLocation] = useState<string>("");
-    const [po, setPO] = useState<string>("");
-    const [image, setImage] = useState<string>("");
-    const [columns, setColumns] = useState<Column[]>([]);
-
-    if (id)
-        setTitle(options?.name ? `Edit ${options.name}` : "Edit Database");
-    else
-        setTitle("New Database");
-    if (isProduction)
-    {
-        if (!isLoggedIn || !auth.getUserProfile().admin)
-        {
-            navigate("/");
-            return <></>;
-        }
-    }
-
-    const isNew: boolean = !id; // If the id is not null, then it is not a new database.
-
-    if (!isNew)
-    {
-        useEffect(() =>
-        {
-            DatabaseRecords.data(id!, true).then(data =>
-            {
-                setOptions(data);
-                setName(data.name);
-                setLocation(data.location);
-                setPO(data.po);
-                setImage(data.image);
-                setColumns(data.options.columns);
-                console.log("Loading database options: ", data);
-            });
-        }, []);
-    }
-
-
-    return (
-        <div className={"flex flex-col"}>
-            <div className={"flex flex-col pb-[128px] mx-8 gap-4"}>
-                <h1 className={"text-3xl m-4 font-bold"}>{isNew ? "New Database" : "Edit Database"}</h1>
-                <FileUploadInput/>
-                <IconList url={image}/>
-                <div className={"flex flex-col gap-4"}>
-                    <Input
-                        label={"Database Name"}
-                        placeholder={"Enter the name of the database"}
-                        radius={"full"}
-                        autoComplete={"off"}
-                        value={name}
-                        onValueChange={setName}
-                    />
-                    <div className={"flex flex-row gap-4"}>
-                        <Input
-                            label={"Location"}
-                            placeholder={"Enter the location of the database"}
-                            radius={"full"}
-                            autoComplete={"off"}
-                            value={location}
-                            onValueChange={setLocation}
-                        />
-                        <Input
-                            label={"Database PO#"}
-                            placeholder={"Enter the PO# of the database"}
-                            radius={"full"}
-                            autoComplete={"off"}
-                            value={po}
-                            onValueChange={setPO}
-                        />
-                    </div>
-                </div>
-
-                <ColumnsList columns={columns}/>
-                <h3 className={"text-2xl"}>Extra Options</h3>
-                <AllowInventorying/>
-                <CanPrintLabel/>
-            </div>
-            <div className={"flex flex-row items-center justify-end gap-2 fixed bottom-0 h-20 w-full pr-4 z-10 backdrop-blur-lg backdrop-saturate-150 bg-background/70"}>
-                <Button radius={"full"} color={"primary"}>Save</Button>
-                <Button radius={"full"} as={Link} href={isNew ? "/" : `/${id}`}>Cancel</Button>
-            </div>
-        </div>
-    );
+    return <></>;
+    // const navigate = useNavigate();
+    // const {isLoggedIn, auth} = useAuth();
+    // const id = useParams().id;
+    // const [options, setOptions] = useState<DatabaseData | null>(null);
+    // const [name, setName] = useState<string>("");
+    // const [location, setLocation] = useState<string>("");
+    // const [po, setPO] = useState<string>("");
+    // const [image, setImage] = useState<string>("");
+    // const [columns, setColumns] = useState<Column[]>([]);
+    //
+    // if (id)
+    //     setTitle(options?.name ? `Edit ${options.name}` : "Edit Database");
+    // else
+    //     setTitle("New Database");
+    // if (isProduction)
+    // {
+    //     if (!isLoggedIn || !auth.getUserProfile().admin)
+    //     {
+    //         navigate("/");
+    //         return <></>;
+    //     }
+    // }
+    //
+    // const isNew: boolean = !id; // If the id is not null, then it is not a new database.
+    //
+    // if (!isNew)
+    // {
+    //     useEffect(() =>
+    //     {
+    //         DatabaseRecords.data(id!, true).then(data =>
+    //         {
+    //             setOptions(data);
+    //             setName(data.name);
+    //             setLocation(data.location);
+    //             setPO(data.po);
+    //             setImage(data.image);
+    //             setColumns(data.options.columns);
+    //             console.log("Loading database options: ", data);
+    //         });
+    //     }, []);
+    // }
+    //
+    //
+    // return (
+    //     <div className={"flex flex-col"}>
+    //         <div className={"flex flex-col pb-[128px] mx-8 gap-4"}>
+    //             <h1 className={"text-3xl m-4 font-bold"}>{isNew ? "New Database" : "Edit Database"}</h1>
+    //             <FileUploadInput/>
+    //             <IconList url={image}/>
+    //             <div className={"flex flex-col gap-4"}>
+    //                 <Input
+    //                     label={"Database Name"}
+    //                     placeholder={"Enter the name of the database"}
+    //                     radius={"full"}
+    //                     autoComplete={"off"}
+    //                     value={name}
+    //                     onValueChange={setName}
+    //                 />
+    //                 <div className={"flex flex-row gap-4"}>
+    //                     <Input
+    //                         label={"Location"}
+    //                         placeholder={"Enter the location of the database"}
+    //                         radius={"full"}
+    //                         autoComplete={"off"}
+    //                         value={location}
+    //                         onValueChange={setLocation}
+    //                     />
+    //                     <Input
+    //                         label={"Database PO#"}
+    //                         placeholder={"Enter the PO# of the database"}
+    //                         radius={"full"}
+    //                         autoComplete={"off"}
+    //                         value={po}
+    //                         onValueChange={setPO}
+    //                     />
+    //                 </div>
+    //             </div>
+    //
+    //             <ColumnsList columns={columns}/>
+    //             <h3 className={"text-2xl"}>Extra Options</h3>
+    //             <AllowInventorying/>
+    //             <CanPrintLabel/>
+    //         </div>
+    //         <div className={"flex flex-row items-center justify-end gap-2 fixed bottom-0 h-20 w-full pr-4 z-10 backdrop-blur-lg backdrop-saturate-150 bg-background/70"}>
+    //             <Button radius={"full"} color={"primary"}>Save</Button>
+    //             <Button radius={"full"} as={Link} href={isNew ? "/" : `/${id}`}>Cancel</Button>
+    //         </div>
+    //     </div>
+    // );
 }
