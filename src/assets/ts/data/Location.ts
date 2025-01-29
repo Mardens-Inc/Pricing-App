a7e3fa8050f17e526425df31c6138c7c336f50dd
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-29 15:30:15 -0500

    Add record fetching and search functionality to Location
    Introduced methods to retrieve records and perform searches with flexible query options in the Location class. Added utility to construct URLs based on provided record and search parameters. Adjusted API endpoint formatting for consistency.

--- src/assets/ts/data/Location.ts
+++ src/assets/ts/data/Location.ts
@@ -1,5 +1,24 @@
 import $ from "jquery";
 
+export interface RecordOptions
+{
+    limit?: number;
+    offset?: number;
+    sort?: string;
+    ascending?: boolean;
+}
+
+export interface RecordSearchOptions extends RecordOptions
+{
+    search?: string;
+    columns?: string[];
+}
+
+export type InventoryRecord = {
+    [key: string]: string;
+}
+
+
 export default class Location
 {
     public id: string;
@@ -22,7 +41,7 @@ export default class Location
 
     static async all(): Promise<Location[]>
     {
-        return $.get("/api/list");
+        return $.get("/api/list/");
     }
 
     static async get(id: string): Promise<Location | undefined>
@@ -57,8 +76,38 @@ export default class Location
         {
             return false;
         }
-
     }
 
+    async records(options: RecordOptions = {}): Promise<InventoryRecord[]>
+    {
+        return $.get(optionsToUrl(this.id, options).toString());
+    }
 
+    async search(options: RecordSearchOptions = {}): Promise<InventoryRecord[]>
+    {
+        return $.get(optionsToUrl(this.id, options).toString());
+    }
+}
+
+function optionsToUrl(id: string, options: RecordOptions | RecordSearchOptions): URL
+{
+    let url: URL = new URL(`/api/list/${id}`);
+
+    if ("search" in options || "columns" in options)
+    {
+        if (options.search)
+            url.searchParams.append("search", options.search);
+        if (options.columns)
+            url.searchParams.append("query_columns", options.columns.join(","));
+    }
+    if (options.limit)
+        url.searchParams.append("limit", options.limit.toString());
+    if (options.offset)
+        url.searchParams.append("offset", options.offset.toString());
+    if (options.sort)
+        url.searchParams.append("sort_by", options.sort);
+    if (options.ascending)
+        url.searchParams.append("sort_order", options.ascending ? "ASC" : "DESC");
+
+    return url;
 }
