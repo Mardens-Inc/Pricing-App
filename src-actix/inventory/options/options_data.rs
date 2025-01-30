0a7723dc37a1075cc91981e8f28db7f1af5baf24
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-30 14:35:51 -0500

    Refactor: Remove unused serde rename attributes
    Simplified the code by removing unnecessary `serde(rename)` attributes. This improves readability and reduces potential redundancy in future updates.

--- src-actix/inventory/options/options_data.rs
+++ src-actix/inventory/options/options_data.rs
@@ -1,10 +1,9 @@
+use crate::print_options_data::PrintForm;
 use serde::Serialize;
 use serde_derive::Deserialize;
-use crate::print_options_data::PrintForm;
 
 #[derive(Serialize, Deserialize)]
 pub struct InventoryOptions {
-    #[serde(rename = "print-form")]
     pub print_form: Option<Vec<PrintForm>>,
     pub inventorying: Option<Inventorying>,
     pub show_year_input: bool,
@@ -12,11 +11,7 @@ pub struct InventoryOptions {
 }
 #[derive(Serialize, Deserialize)]
 pub struct Inventorying {
-    #[serde(rename = "add-if-missing")]
     pub add_if_missing: bool,
-    #[serde(rename = "remove-if-zero")]
     pub remove_if_zero: bool,
-    #[serde(rename = "allow-additions")]
     pub allow_additions: bool,
 }
-
