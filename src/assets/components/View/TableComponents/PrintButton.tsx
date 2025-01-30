f42174ed75aa0796599e22e22fbba6fa03709635
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-30 11:24:50 -0500

    Remove PrintPercentagesButton and refactor printing logic
    Eliminated the usage of `PrintPercentagesButton` and updated the `PrintButton` to handle printing directly. Adjusted column mapping to use `c.name` instead of `c.real_name` for consistency, and streamlined dependencies by using `Column` and `PrintForm` for better structure and clarity.

--- src/assets/components/View/TableComponents/PrintButton.tsx
+++ src/assets/components/View/TableComponents/PrintButton.tsx
@@ -2,8 +2,9 @@ import {Button, getKeyValue, Tooltip} from "@heroui/react";
 import {OpenPrintWindow} from "../../../ts/printer.ts";
 import $ from "jquery";
 import {RowValue} from "../InventoryTable.tsx";
-import PrintPercentagesButton from "./PrintPercentagesButton.tsx";
 import {Icon} from "@iconify/react";
+import Column from "../../../ts/data/Column.ts";
+import {PrintForm} from "../../../ts/data/Options.ts";
 
 export interface PrintButtonProps
 {
@@ -15,13 +16,11 @@ export interface PrintButtonProps
 
 export default function PrintButton(props: PrintButtonProps)
 {
-    if (props.printOptions.percentages && props.printOptions.percentages.length > 0)
-        return <PrintPercentagesButton {...props}/>;
     return (
         <Tooltip content={"Print"} classNames={{base: "pointer-events-none"}} closeDelay={0}>
             <Button id={`print-button-${props.databaseId}-${props.row.id}`} radius={"full"} className={"min-w-0 w-12 h-12"} data-print-form onPress={() =>
             {
-                const rowData = props.columns.map(c => ({value: getKeyValue(props.row, c.real_name), attributes: c.attributes})) as RowValue[];
+                const rowData = props.columns.map(c => ({value: getKeyValue(props.row, c.name), attributes: c.attributes})) as RowValue[];
                 OpenPrintWindow(props.databaseId, rowData, props.printOptions, +($(`tr#${props.row.id}`).attr("data-department") ?? -1));
             }}><Icon icon={"mage:printer-fill"}/></Button>
         </Tooltip>
