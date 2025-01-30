ce2de227e1e7cfa775c9a8af3ec929fc79d56198
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-30 15:33:47 -0500

    Add PrintDropdown component to handle printing options
    This new component provides a dropdown interface for printing options, utilizing reusable Button, Tooltip, and Dropdown menus. It fetches row data and dynamically calculates prices or markups based on selected print forms, integrating seamlessly with the existing inventory table structure.

---
+++ src/assets/components/View/TableComponents/PrintDropdown.tsx
@@ -0,0 +1,103 @@
+import {Badge, Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, getKeyValue, Tooltip} from "@heroui/react";
+import {RowValue} from "../InventoryTable.tsx";
+import {Icon} from "@iconify/react";
+import Column from "../../../ts/data/Column.ts";
+import {PrintForm} from "../../../ts/data/Options.ts";
+import {OpenPrintWindow} from "../../../ts/printer.ts";
+
+export interface PrintDropdownProps
+{
+    databaseId: string;
+    row: any;
+    columns: Column[];
+    printOptions: PrintForm[];
+}
+
+export default function PrintDropdown(props: PrintDropdownProps)
+{
+    const rowData = props.columns.map(c => ({value: getKeyValue(props.row, c.name), attributes: c.attributes})) as RowValue[];
+    let price = +((rowData.find(v => v.attributes.includes("price")) ?? {}).value ?? 0);
+
+    return (
+        <Dropdown>
+            <DropdownTrigger>
+                <div>
+                    <Badge content={"+"} color={"primary"} showOutline={false}>
+                        <Tooltip content={"Show Print Options"} classNames={{base: "pointer-events-none"}} closeDelay={0}>
+                            <Button
+                                radius={"full"}
+                                className={"min-w-0 w-12 h-12"}
+                                onPressStart={e => e.continuePropagation()}
+                            >
+                                <Icon icon={"mage:printer-fill"}/>
+                            </Button>
+                        </Tooltip>
+                    </Badge>
+                </div>
+            </DropdownTrigger>
+            <DropdownMenu>
+                {props.printOptions.map(form =>
+                {
+                    let mp = +((rowData.find(v => v.attributes.includes("mp")) ?? {}).value ?? 0);
+
+                    if (form.percentOffRetail)
+                    {
+                        mp = price * (form.percentOffRetail / 100);
+                    }
+
+                    return <DropdownItem
+                        key={`print-item-${form.id}-${form.hint}`} id={`print-item-${form.id}-${form.hint}`}
+                        endContent={<Icon icon={"mage:printer-fill"}/>}
+                        onPressStart={e => e.continuePropagation()}
+                        onPress={() =>
+                        {
+                            OpenPrintWindow(props.databaseId, rowData, form, +($(`tr#${props.row.id}`).attr("data-department") ?? -1), mp);
+                        }}
+                    >
+                        <div className={"flex flex-row items-center border-b-1 border-white/10"}>
+                            <div className={"w-full"}>Print {form.hint}</div>
+                            <div className={"ml-auto text-tiny opacity-50"}>${mp.toFixed(2)}</div>
+                        </div>
+                    </DropdownItem>;
+                })}
+            </DropdownMenu>
+        </Dropdown>
+    );
+}
+
+//        <Dropdown>
+//             <DropdownTrigger>
+//                 <div>
+//                     <Badge content={"%"} color={"primary"} showOutline={false}>
+//                         <Tooltip content={"Show Print Options"} classNames={{base: "pointer-events-none"}} closeDelay={0}>
+//                             <Button
+//                                 radius={"full"}
+//                                 className={"min-w-0 w-12 h-12"}
+//                                 onPressStart={e => e.continuePropagation()}
+//                             >
+//                                 <Icon icon={"mage:printer-fill"}/>
+//                             </Button>
+//                         </Tooltip>
+//                     </Badge>
+//                 </div>
+//             </DropdownTrigger>
+//             <DropdownMenu>
+//                 {pricePercentage.map((item, index) => (
+//                     <DropdownItem
+//                         key={index}
+//                         onPressStart={e => e.continuePropagation()}
+//                         onPress={
+//                             () =>
+//                             {
+//                                 OpenPrintWindow(props.databaseId, rowData, props.printOptions, +($(`tr#${props.row.id}`).attr("data-department") ?? -1), item.price);
+//                             }
+//                         }
+//                     >
+//                         <div className={"flex flex-row items-center border-b-1 border-white/10"}>
+//                             <div className={"w-full"}>Print {item.percentage}%</div>
+//                             <div className={"ml-auto text-tiny opacity-50"}>${item.price.toFixed(2)}</div>
+//                         </div>
+//                     </DropdownItem>
+//                 ))}
+//             </DropdownMenu>
+//         </Dropdown>
