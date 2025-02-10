5c7980e2b2686664e97ba8f25a0fed6f86764ffc
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-10 11:49:19 -0500

    Add new error variants and conversion implementations
    Enhanced the `Error` enum with additional variants for better error categorization, including `Other`, `Anyhow`, and `HeaderParse`. Implemented `From` conversions for `anyhow::Error`, `std::io::Error`, and `ToStrError`, improving error handling flexibility and integration.

--- src-actix/http_error.rs
+++ src-actix/http_error.rs
@@ -1,24 +1,50 @@
+use actix_web::http::header::ToStrError;
 use actix_web::http::StatusCode;
 use actix_web::{HttpResponse, ResponseError};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[allow(dead_code)]
     #[error("an unspecified internal error occurred: {0}")]
-    InternalError(#[from] anyhow::Error),
+    InternalError(anyhow::Error),
+    #[allow(dead_code)]
+    #[error(transparent)]
+    Other(anyhow::Error),
+    #[error("an error has occurred: {0:?}")]
+    Anyhow(anyhow::Error),
+    #[error("unable to parse headers: {0:?}")]
+    HeaderParse(ToStrError),
 }
 
 impl ResponseError for Error {
     fn status_code(&self) -> StatusCode {
         match &self {
-            Self::InternalError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::InternalError(_) | Self::Other(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            _ => StatusCode::BAD_REQUEST,
         }
     }
 
-    fn error_response(&self) -> HttpResponse { 
+    fn error_response(&self) -> HttpResponse {
         HttpResponse::build(self.status_code()).body(self.to_string())
     }
 }
 
+impl From<anyhow::Error> for Error {
+    fn from(err: anyhow::Error) -> Self {
+        Error::Anyhow(err)
+    }
+}
 
+impl From<ToStrError> for Error {
+    fn from(err: ToStrError) -> Self {
+        Error::HeaderParse(err)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::Anyhow(anyhow::Error::new(err))
+    }
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
