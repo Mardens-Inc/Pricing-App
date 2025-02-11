c71a6125ec24c0d98c9b5ed99cc81d0c1eeeba9c
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-02-11 11:59:06 -0500

    Refactor and update icon API tests
    Reorganized test cases to improve clarity and ensure proper alignment with API functionalities. Added more specific test coverage for missing icons, file uploads, overwrite scenarios, and retrieving icons. Adjusted assertions and debug logs to align with new test logic and API behaviors.

--- tests/icons.rs
+++ tests/icons.rs
@@ -7,62 +7,6 @@ mod tests {
     use std::path::Path;
 
     #[actix_web::test]
-    async fn test_get_icons() {
-        std::env::set_var("RUST_LOG", "debug");
-        _ = env_logger::try_init();
-        std::env::set_current_dir(Path::new("target/dev-env"))
-            .expect("Failed to set current directory");
-        initialize_asset_directories().unwrap();
-
-        let app = test::init_service(
-            App::new().configure(|cfg| configure(cfg.service(web::scope("/api")))),
-        )
-        .await;
-
-        let req = test::TestRequest::get().uri("/icons").to_request();
-        let resp = test::call_service(&app, req).await;
-        let status = resp.status();
-        if status != 200 {
-            let body = test::read_body(resp).await.to_vec();
-            let body = body.as_slice();
-            let body = String::from_utf8(body.to_vec()).unwrap();
-            error!(
-                "Get icons response failed with status: {} and body: {:?}",
-                status, body
-            )
-        }
-        assert_eq!(status, 200)
-    }
-
-    #[actix_web::test]
-    async fn test_get_icon() {
-        std::env::set_var("RUST_LOG", "debug");
-        _ = env_logger::try_init();
-        std::env::set_current_dir(Path::new("target/dev-env"))
-            .expect("Failed to set current directory");
-        initialize_asset_directories().unwrap();
-        let app = test::init_service(
-            App::new().configure(|cfg| configure(cfg.service(web::scope("/api")))),
-        )
-        .await;
-        let req = test::TestRequest::get()
-            .uri("/icons/amazon.png")
-            .to_request();
-        let resp = test::call_service(&app, req).await;
-        let status = resp.status();
-        let body = test::read_body(resp).await.to_vec();
-        if status != 200 {
-            let body = body.as_slice();
-            let body = String::from_utf8(body.to_vec()).unwrap();
-            error!(
-                "Get icon response failed with status: {} and body: {:?}",
-                status, body
-            )
-        }
-        assert_eq!(status, 200);
-        assert!(!body.is_empty(), "Response body is empty");
-    }
-    #[actix_web::test]
     async fn test_get_missing_icon() {
         std::env::set_var("RUST_LOG", "debug");
         _ = env_logger::try_init();
@@ -175,5 +119,81 @@ mod tests {
             status, body
         );
         assert_eq!(status, 400);
+    }
+    #[actix_web::test]
+    async fn test_get_icons() {
+        std::env::set_var("RUST_LOG", "debug");
+        _ = env_logger::try_init();
+        std::env::set_current_dir(Path::new("target/dev-env"))
+            .expect("Failed to set current directory");
+        initialize_asset_directories().unwrap();
+
+        let app = test::init_service(
+            App::new().configure(|cfg| configure(cfg.service(web::scope("/api")))),
+        )
+        .await;
+
+        // Read the test file
+        let file =
+            std::fs::read("../../tests/assets/icon.jpg").expect("Unable to read test icon file");
+        let req = test::TestRequest::post()
+            .uri("/icons?overwrite=true")
+            .set_payload(file)
+            .insert_header(("content-disposition", "attachment; filename=test_cat.jpg"))
+            .to_request();
+        _ = test::call_service(&app, req).await;
+
+        let req = test::TestRequest::get().uri("/icons").to_request();
+        let resp = test::call_service(&app, req).await;
+        let status = resp.status();
+        if status != 200 {
+            let body = test::read_body(resp).await.to_vec();
+            let body = body.as_slice();
+            let body = String::from_utf8(body.to_vec()).unwrap();
+            error!(
+                "Get icons response failed with status: {} and body: {:?}",
+                status, body
+            )
+        }
+        assert_eq!(status, 200)
+    }
+    #[actix_web::test]
+    async fn test_get_icon() {
+        std::env::set_var("RUST_LOG", "debug");
+        _ = env_logger::try_init();
+        std::env::set_current_dir(Path::new("target/dev-env"))
+            .expect("Failed to set current directory");
+        initialize_asset_directories().unwrap();
+        let app = test::init_service(
+            App::new().configure(|cfg| configure(cfg.service(web::scope("/api")))),
+        )
+        .await;
+
+        // Read the test file
+        let file =
+            std::fs::read("../../tests/assets/icon.jpg").expect("Unable to read test icon file");
+        let req = test::TestRequest::post()
+            .uri("/icons?overwrite=true")
+            .set_payload(file)
+            .insert_header(("content-disposition", "attachment; filename=test_cat.jpg"))
+            .to_request();
+        _ = test::call_service(&app, req).await;
+        
+        let req = test::TestRequest::get()
+            .uri("/icons/test_cat.jpg")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        let status = resp.status();
+        let body = test::read_body(resp).await.to_vec();
+        if status != 200 {
+            let body = body.as_slice();
+            let body = String::from_utf8(body.to_vec()).unwrap();
+            error!(
+                "Get icon response failed with status: {} and body: {:?}",
+                status, body
+            )
+        }
+        assert_eq!(status, 200);
+        assert!(!body.is_empty(), "Response body is empty");
     }
 }
