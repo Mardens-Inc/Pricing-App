dd45b2da478567d37e7221acf807cea3c9ae61ea
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-03-12 16:22:54 -0400

    Add user authentication checks and dropdown menu options
    Integrated `isLoggedIn` checks to conditionally render actions based on user authentication. Introduced a dropdown menu providing additional record options such as history, duplication, and deletion, improving functionality and user experience.

--- src/assets/components/View/InventoryTable.tsx
+++ src/assets/components/View/InventoryTable.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useMemo, useState} from "react";
-import {Button, cn, getKeyValue, SortDescriptor, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@heroui/react";
+import {Button, cn, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, getKeyValue, SortDescriptor, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@heroui/react";
 import {useSearch} from "../../providers/SearchProvider";
 import {useNavigate, useParams} from "react-router-dom";
 import PrintButton from "./TableComponents/PrintButton";
@@ -9,6 +9,7 @@ import Column from "../../ts/data/Column";
 import Options from "../../ts/data/Options";
 import Location, {InventoryRecord} from "../../ts/data/Location";
 import {useNewRecordModal} from "../../providers/NewRecordModalProvider";
+import {useAuth} from "../../providers/AuthProvider.tsx";
 
 interface InventoryTableProps
 {
@@ -29,6 +30,7 @@ export default function InventoryTable(props: InventoryTableProps)
     const [items, setItems] = useState<InventoryRecord[]>([]);
     const [location, setLocation] = useState<Location | undefined>(undefined);
     const [columns, setColumns] = useState<Column[]>([]);
+    const {isLoggedIn} = useAuth();
 
     const [searchColumns, setSearchColumns] = useState<string[]>([]);
     const [_primaryKey, setPrimaryKey] = useState<string>("");
@@ -114,7 +116,7 @@ export default function InventoryTable(props: InventoryTableProps)
             // Abort the fetch on component unmount or dependency change
             abortController.abort();
         };
-    }, [search, location, sortDescriptor]);
+    }, [search, location, sortDescriptor, isLoggedIn]);
 
     useEffect(() =>
     {
@@ -175,35 +177,37 @@ export default function InventoryTable(props: InventoryTableProps)
                         }),
                     <TableCell key={`${row.id}-actions`}>
                         <div className={"flex flex-row gap-2"}>
-                            <Button
-                                radius={"full"}
-                                className={
-                                    "h-12 w-12 aspect-square p-0 min-w-0 text-[1rem] my-auto"
-                                }
-                                onPress={async () =>
-                                {
-                                    const record = await location?.single(row.id);
-                                    if (record)
-                                    {
-                                        edit(record, (item) =>
-                                        {
-                                            for (const c of Object.keys(row) as (keyof InventoryRecord)[])
-                                            {
-                                                const column = c as string;
-                                                const new_record = item[column];
-                                                const old_record = row[column];
-                                                const columnElement = $(`#${row.id}-${column.replace(/\s/g, "").toLowerCase()}`);
-                                                if (columnElement.length > 0 && new_record !== old_record)
-                                                {
-                                                    columnElement.html(`<span>${new_record}</span>`);
-                                                }
-                                            }
-                                        });
+                            {isLoggedIn &&
+                                <Button
+                                    radius={"full"}
+                                    className={
+                                        "h-12 w-12 aspect-square p-0 min-w-0 text-[1rem] my-auto"
                                     }
-                                }}
-                            >
-                                <Icon icon={"mage:edit-fill"}/>
-                            </Button>
+                                    onPress={async () =>
+                                    {
+                                        const record = await location?.single(row.id);
+                                        if (record)
+                                        {
+                                            edit(record, (item) =>
+                                            {
+                                                for (const c of Object.keys(row) as (keyof InventoryRecord)[])
+                                                {
+                                                    const column = c as string;
+                                                    const new_record = item[column];
+                                                    const old_record = row[column];
+                                                    const columnElement = $(`#${row.id}-${column.replace(/\s/g, "").toLowerCase()}`);
+                                                    if (columnElement.length > 0 && new_record !== old_record)
+                                                    {
+                                                        columnElement.html(`<span>${new_record}</span>`);
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }}
+                                >
+                                    <Icon icon={"mage:edit-fill"}/>
+                                </Button>
+                            }
                             {(() =>
                             {
                                 if (props.options.printForm && props.options.isPrintingEnabled())
@@ -219,6 +223,39 @@ export default function InventoryTable(props: InventoryTableProps)
                                 }
                                 return <></>;
                             })()}
+
+                            {isLoggedIn &&
+                                <Dropdown>
+                                    <DropdownTrigger>
+                                        <Button
+                                            radius={"full"}
+                                            className={
+                                                "h-12 w-12 aspect-square p-0 min-w-0 text-[1rem] my-auto"
+                                            }
+                                        >
+                                            <Icon icon="mage:dots"/>
+                                        </Button>
+                                    </DropdownTrigger>
+                                    <DropdownMenu>
+                                        <DropdownSection title={"record options"} showDivider>
+                                            <DropdownItem key={`history-${id}-${row.id}`} endContent={<Icon icon="mage:clock-fill"/>}>
+                                                View History
+                                            </DropdownItem>
+                                            <DropdownItem key={`copy-${id}-${row.id}`} endContent={<Icon icon="mage:copy-fill"/>}>
+                                                Duplicate Record
+                                            </DropdownItem>
+
+                                        </DropdownSection>
+                                        <DropdownSection title={"danger zone"} className={"text-danger"} key={"danger-zone"}>
+                                            <DropdownItem key={`delete-${id}-${row.id}`} endContent={<Icon icon="mage:trash-3-fill"/>} color={"danger"}>
+                                                Delete Record
+                                            </DropdownItem>
+                                        </DropdownSection>
+                                    </DropdownMenu>
+                                </Dropdown>
+
+                            }
+
                         </div>
                     </TableCell>
                 ]}
